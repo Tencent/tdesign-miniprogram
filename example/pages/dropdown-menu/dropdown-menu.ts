f59e8d49a757be002b9bb11a064fa7c6152f383e
caihuiji <caihuijigood@gmail.com>
2021-04-10 18:47:38 +0800

    feat:bugfix

--- example/pages/dropdown-menu/dropdown-menu.ts
+++ example/pages/dropdown-menu/dropdown-menu.ts
@@ -10,6 +10,72 @@ const disabledArr = [...emptyArr].map((_, i) => ({
   disabled: (i + 1) % 3 === 0,
 }));
 
+
+const treeOption = [{
+  title: '北京市',
+  value: 'beijing',
+  options: [{
+    title: '北京市',
+    value: 'beijing',
+    options: [
+      { title: '东城区', value: 'dongcheng' },
+      { title: '西城区', value: 'xicheng' },
+      { title: '朝阳区', value: 'chaoyang' },
+      { title: '丰台区', value: 'fengtai' },
+      { title: '石景山区', value: 'shijingshan' },
+      { title: '海淀区', value: 'haidian' },
+      { title: '门头沟区', value: 'mentougou' },
+      { title: '房山区', value: 'fangshan' },
+      { title: '通州区', value: 'tongzhou' },
+      { title: '顺义区', value: 'shunyi' },
+    ],
+  }],
+}, {
+  title: '天津市',
+  value: 'tianjin',
+  options: [{
+    title: '天津市',
+    value: 'tianjin',
+    options: [
+      { title: '和平区', value: 'heping' },
+      { title: '河东区', value: 'hedong' },
+      { title: '河西区', value: 'hexi' },
+    ],
+  }],
+}, {
+  title: '河北省',
+  value: 'hebei',
+  options: [
+    {
+      title: '石家庄市',
+      value: 'shijiazhuang',
+      options: [
+        { title: '长安区', value: 'changan' },
+        { title: '桥西区', value: 'qiaoxi' },
+        { title: '新华区', value: 'xinhua' },
+        { title: '井陉矿区', value: 'jingjingkuang' },
+        { title: '裕华区', value: 'yuhua' },
+        { title: '藁城区', value: 'gaocheng' },
+      ],
+    },
+    {
+      title: '唐山市',
+      value: 'tangshan',
+      options: [
+        { title: '曹妃甸区', value: 'caofeidian' },
+        { title: '丰南区', value: 'fengnan' },
+        { title: '丰润区', value: 'fengrun' },
+        { title: '古冶区', value: 'guye' },
+        { title: '开平区', value: 'kaiping' },
+        { title: '乐亭区', value: 'laoting' },
+      ],
+    },
+  ],
+}];
+
+const treeValue1 = null;
+const treeValue2 = ['tianjin', 'tianjin', ['hedong', 'hexi']];
+
 Page({
   data: {
     tab: 0,
@@ -19,6 +85,9 @@ Page({
     selectedM: ['option_1', 'option_3'],
     optionsD: disabledArr,
     selectedD: 'option_1',
+    treeOption,
+    treeValue1,
+    treeValue2
   },
   multiSelected(e) {
     this.setData({
