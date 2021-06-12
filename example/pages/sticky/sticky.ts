13deea2b136384d9e0fd36a78e5d84eb4387d8db
amberlwan <2197131426@qq.com>
2021-06-12 10:31:58 +0800

    feat:新增sticky组件

--- example/pages/sticky/sticky.ts
+++ example/pages/sticky/sticky.ts
@@ -1,1 +1,14 @@
-Page({});
+Page({
+  data: {
+    contentRef: null,
+    container: null,
+  },
+  onLoad() {
+    this.setData({
+      // @ts-ignore
+      contentRef: () => this.createSelectorQuery().select('.content'),
+      // @ts-ignore
+      container: () => wx.createSelectorQuery().select('.box-c'),
+    });
+  },
+});
