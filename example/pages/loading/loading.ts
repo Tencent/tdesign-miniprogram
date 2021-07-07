56828591bfb8ce2ce98caef69b90f0383b42a887
JJunYang <jjy_yang@163.com>
2021-07-07 17:06:25 +0800

    feat:Loading组件整合

--- example/pages/loading/loading.ts
+++ example/pages/loading/loading.ts
@@ -1,62 +1,19 @@
 Page({
   data: {
-    vertical: true,
-    error: false,
-    size: '48px',
-    showType: 'all',
-    showTypeIdx: 0,
-    layout: 'default',
     barLoadingTimeOut: null,
-    loading: true,
+    loading: false,
+    progress: -1,
   },
-  toggleVertical() {
-    this.resetDefault();
+  showProgressBar() {
+    const timeout = setTimeout(() => {
+      this.setData({ loading: false });
+    }, 10000);
     this.setData({
-      vertical: !this.data.vertical,
-    });
-  },
-  toggleError() {
-    this.setData({
-      error: !this.data.error,
-    });
-  },
-  toggleSize() {
-    this.resetDefault();
-    this.setData({
-      size: this.data.size == '48px' ? '30px' : '48px',
-    });
-  },
-  toggleLayout() {
-    if (this.data.layout == 'default') {
-      const timeout = setTimeout(() => {
-        this.setData({ loading: false });
-      }, 10000);
-      this.setData({
-        layout: 'bar',
-        barLoadingTimeOut: timeout,
-      });
-      this.changeNavBarColor('#000000', '#ffffff');
-    } else {
-      this.resetDefault();
-    }
-  },
-  toggleShowType() {
-    this.resetDefault();
-    const types = ['all', 'icon-only', 'text-only'];
-    const index = (this.data.showTypeIdx + 1) % types.length;
-    this.setData({
-      showType: types[index],
-      showTypeIdx: index,
-    });
-  },
-  resetDefault() {
-    clearTimeout(this.data.barLoadingTimeOut);
-    this.changeNavBarColor('#ffffff', '#0052d9');
-    this.setData({
-      layout: 'default',
-      error: false,
+      progress: -1,
       loading: true,
+      barLoadingTimeOut: timeout,
     });
+    this.changeNavBarColor('#000000', '#ffffff');
   },
   changeNavBarColor(frontColor, backgroundColor) {
     wx.setNavigationBarColor({
@@ -66,6 +23,12 @@ Page({
         duration: 500,
         timingFunc: 'easeIn',
       },
+    });
+  },
+
+  reloadPage() {
+    wx.redirectTo({
+      url: '/pages/loading/loading',
     });
   },
 });
--- src/loading/loading.ts
+++ src/loading/loading.ts
@@ -1,58 +1,58 @@
-import TComponent from '../common/component';
+import { SuperComponent, wxComponent } from '../common/src/index';
 import config from '../common/config';
 const { prefix } = config;
 const name = `${prefix}-loading`;
 
-TComponent({
-  data: {
+@wxComponent()
+export default class extends SuperComponent {
+  externalClasses = ['t-class', 't-class-spinner', 't-class-circular-mask', 't-class-bar'];
+  data = {
     classPrefix: name,
-  },
-  properties: {
-    layout: {
+  };
+  properties = {
+    type: {
       type: String,
-      value: 'default', // 'default' | 'full' | 'bar'
+      value: 'circular',
     },
+    vertical: Boolean,
     size: {
       type: String,
-      value: 'medium',
+      value: '40rpx',
     },
     textSize: {
       type: String,
-      value: '14px',
+      value: '24rpx',
     },
-    textPadding: {
+    color: {
       type: String,
-      value: '0',
+      value: '#000000',
     },
-    vertical: {
-      type: Boolean,
-      value: true,
-    },
-    type: {
+    textColor: {
       type: String,
-      value: 'circle',
+      value: '#000000',
     },
+    backgroundColor: {
+      type: String,
+      value: '',
+    },
+    reverse: Boolean,
+    duration: {
+      type: Number,
+      value: '0.8',
+    },
+    paused: Boolean,
     error: {
       type: Boolean,
       value: false,
-    },
-    showType: {
-      type: String,
-      value: 'all', // 'all' | 'text-only' | 'icon-only'
-    },
-    title: {
-      type: String,
-      value: '加载中',
     },
     progress: {
       type: Number,
       value: -1,
     },
     loading: Boolean,
-  },
-  methods: {
-    reloadClick() {
-      this.triggerEvent('reload');
-    },
-  },
-});
+  };
+
+  reloadClick() {
+    this.triggerEvent('reload');
+  }
+}
