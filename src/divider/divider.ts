6c2f059a24c87aff53d87f201e4e9ce7f76bae32
Anlyyao <anly_yaw@163.com>
2021-08-05 17:17:36 +0800

    feat(divider): update divider

--- src/divider/divider.ts
+++ src/divider/divider.ts
@@ -1,11 +1,11 @@
 import { SuperComponent, wxComponent } from '../common/src/index';
 import config from '../common/config';
+import props from './props';
 const { prefix } = config;
 const name = `${prefix}-divider`;
 @wxComponent()
 export default class Divider extends SuperComponent {
   externalClasses = ['t-class', 't-class-content'];
-
   options = {
     addGlobalClass: true,
     multipleSlots: true,
@@ -13,32 +13,7 @@ export default class Divider extends SuperComponent {
   /**
    * 组件的属性列表
    */
-  properties = {
-    dashed: {
-      type: Boolean,
-      value: false,
-    },
-    hairline: {
-      type: Boolean,
-      value: false,
-    },
-    contentPosition: {
-      type: String,
-      value: '',
-    },
-    borderColor: {
-      type: String,
-      value: '',
-    },
-    textColor: {
-      type: String,
-      value: '',
-    },
-    customStyle: {
-      type: String,
-      value: '',
-    },
-  };
+  properties = props;
   /**
    * 组件的初始数据
    */
---
+++ src/divider/type.ts
@@ -0,0 +1,60 @@
+/**
+ * 该文件为脚本自动生成文件，请勿随意修改。如需修改请联系 PMC
+ * updated at 2021-08-05 13:36:03
+ * */
+
+export interface TdDividerProps {
+  /**
+   * 文本位置（仅在水平分割线有效）
+   * @default center
+   */
+  align?: {
+    type: StringConstructor;
+    value?: 'left' | 'right' | 'center';
+  };
+  /**
+   * 子元素
+   */
+  content?: {
+    type: StringConstructor;
+    value?: string;
+  };
+  /**
+   * 是否虚线（仅在水平分割线有效）
+   * @default false
+   */
+  dashed?: {
+    type: BooleanConstructor;
+    value?: boolean;
+  };
+  /**
+   * 组件类名，分别用于设置 组件外层类名、分隔线类名 等
+   */
+  externalClasses?: {
+    type: ArrayConstructor;
+    value?: ['t-class', 't-class-line', 't-class-content'];
+  };
+  /**
+   * 分隔线颜色
+   * @default ''
+   */
+  lineColor?: {
+    type: StringConstructor;
+    value?: string;
+  };
+  /**
+   * 分隔线类型有两种：水平和垂直
+   * @default horizontal
+   */
+  theme?: {
+    type: StringConstructor;
+    value?: 'horizontal' | 'vertical';
+  };
+  /**
+   * 组件样式
+   */
+  tStyle?: {
+    type: ObjectConstructor;
+    value?: Record<string, any>;
+  };
+}
