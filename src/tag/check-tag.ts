fc02b3045b1279c34fcdd30fb770b61a56c4f8c9
LeeJim <273743552@qq.com>
2021-09-01 11:06:44 +0800

    feat: merge tag

--- src/tag/check-tag.ts
+++ src/tag/check-tag.ts
@@ -1,53 +1,19 @@
-import TComponent from '../common/component';
+import { wxComponent, SuperComponent } from '../common/src/index';
 import config from '../common/config';
+import props from './check-tag-props';
 const { prefix } = config;
 const name = `${prefix}-tag`;
 
-export enum TagSize {
-  Large = 'large',
-  Medium = 'medium',
-  Small = 'small',
-}
-
-export enum TagShape {
-  Square = 'square',
-  Round = 'round',
-  Circle = 'circle',
-}
-TComponent({
-  data: {
+@wxComponent()
+export default class CheckTag extends SuperComponent {
+  data = {
     classPrefix: name,
     classBasePrefix: prefix,
-  },
-  properties: {
-    size: {
-      type: String,
-      value: TagSize.Medium,
-    },
-    icon: {
-      type: String,
-      value: '',
-    },
-    shape: {
-      type: String,
-      value: TagShape.Square,
-    },
-    disabled: {
-      type: Boolean,
-      value: false,
-    },
-    closable: {
-      type: Boolean,
-      value: false,
-    },
-    checked: {
-      type: Boolean,
-      value: false,
-    },
-  },
-  methods: {
+  };
+  properties = props;
+  methods = {
     onClickClose(e: WechatMiniprogram.BaseEvent) {
       this.triggerEvent('close', e);
     },
-  },
-});
+  };
+}
