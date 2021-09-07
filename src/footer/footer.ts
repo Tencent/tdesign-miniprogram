9b76548e628382629011b235b5a53d5944333f2b
Perisiguiendo <1987391831@qq.com>
2021-09-07 19:08:04 +0800

    fix:修复lint报错

--- src/footer/footer.ts
+++ src/footer/footer.ts
@@ -1,12 +1,14 @@
 import { SuperComponent, wxComponent } from '../common/src/index';
 import config from '../common/config';
 import props from './props';
+
 const { prefix } = config;
 const name = `${prefix}-footer`;
 
 @wxComponent()
 export default class Footer extends SuperComponent {
   properties = props;
+
   data = {
     classPrefix: name,
   };
