2ec3acd6abe86a562add6dfe1c2273e96c17d93e
shiyanZhang <zsylewit@163.com>
2021-09-20 15:16:04 +0800

    feat: empty组件-props改为cli生成，优化部分API和externalClass

---
+++ src/empty/props.ts
@@ -0,0 +1,29 @@
+/* eslint-disable */
+
+/**
+ * 该文件为脚本自动生成文件，请勿随意修改。如需修改请联系 PMC
+ * updated at 2021-09-20 14:23:22
+ * */
+
+import { TdEmptyProps } from './type';
+const props: TdEmptyProps = {
+  /** 描述文字 */
+  description: {
+    type: String,
+  },
+  /** 组件类名，分别用于设置 组件外层类名、文本描述类名、图片类名、操作按钮类名 */
+  externalClasses: {
+    type: Array,
+  },
+  /** 图片地址 */
+  image: {
+    type: String,
+  },
+  /** 空页面风格类型（待确认） */
+  theme: {
+    type: String,
+    value: 'default',
+  },
+};
+
+export default props;
