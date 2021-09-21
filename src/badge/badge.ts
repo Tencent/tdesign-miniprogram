1e9df795cd5c420757241f0e8caf62a39b6528c2
zhenzhencai <caizhenzhen94@outlook.com>
2021-09-21 19:48:52 +0800

    feat: 更新badge组件的props cli

--- src/badge/badge.ts
+++ src/badge/badge.ts
@@ -1,63 +1,19 @@
 /*
  * @Author: rileycai
  * @Date: 2021-06-30 11:22:22
- * @LastEditTime: 2021-09-20 20:03:40
+ * @LastEditTime: 2021-09-21 19:48:29
  * @LastEditors: Please set LastEditors
  * @Description: 第一次走查问题修复
  * @FilePath: /tdesign-miniprogram/src/badge/badge.ts
  */
-// import { BADGE_PROPS } from './badge.interface';
 import { SuperComponent, wxComponent } from '../common/src/index';
+import props from './props';
 
-type ShapeType = 'circle' | 'rounded' | 'ribbon';
-type SizeType = 'medium' | 'small';
 @wxComponent()
 export default class Badge extends SuperComponent {
   externalClasses = ['wrapper-class', 'badge-class'];
 
-  properties = {
-    customStyle: String,
-    color: {
-      type: String,
-      value: '#e34d59',
-    },
-    count: {
-      type: Number,
-      value: 0,
-    },
-    dot: {
-      type: Boolean,
-      value: false,
-    },
-    maxCount: {
-      type: Number,
-      value: 99,
-    },
-    content: {
-      type: String,
-      value: '',
-    },
-    size: {
-      type: String,
-      value: 'middle' as SizeType, // 尺寸，支持 'medium', 'small'
-    },
-    shape: {
-      type: String,
-      value: 'circle' as ShapeType, // 尺寸，支持 'circle', 'rounded', 'ribbon'
-    },
-    visible: {
-      type: Boolean,
-      value: false,
-    },
-    offset: {
-      type: Array,
-      value: [0, 0],
-    },
-    hasSlot: {
-      type: Boolean,
-      value: false,
-    },
-  };
+  properties = props;
 
   data = {
     value: '',
--- src/badge/type.ts
+++ src/badge/type.ts
@@ -1,6 +1,6 @@
 /**
  * 该文件为脚本自动生成文件，请勿随意修改。如需修改请联系 PMC
- * updated at 2021-09-20 20:25:14
+ * updated at 2021-09-21 19:47:31
  * */
 
 export interface TdBadgeProps {
