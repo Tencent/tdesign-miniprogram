f216f82d90833f8933c9c9278e10633a5fd6b25f
jin0209 <270246430@qq.com>
2021-09-21 14:28:27 +0800

    fix: icon不显示的异常

--- src/icon/icon.ts
+++ src/icon/icon.ts
@@ -3,7 +3,7 @@ import config from '../common/config';
 import props from './props';
 
 const { prefix } = config;
-const name = `${prefix}-dialog`;
+const name = `${prefix}-icon`;
 
 const sizeKeywordMap = {
   xs: '24rpx',
