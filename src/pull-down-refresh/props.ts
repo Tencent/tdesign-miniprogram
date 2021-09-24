3932af84f4b518d0b9f646881ba995b88231f84e
GAOGAO1994 <GAOGAO1994>
2021-09-24 11:47:04 +0800

    pull-down-refresh优化&api生成

---
+++ src/pull-down-refresh/props.ts
@@ -0,0 +1,40 @@
+/* eslint-disable */
+
+/**
+ * 该文件为脚本自动生成文件，请勿随意修改。如需修改请联系 PMC
+ * updated at 2021-09-24 11:41:27
+ * */
+
+import { TdPullDownRefreshProps } from './type';
+const props: TdPullDownRefreshProps = {
+  /** 加载loading样式 */
+  externalClasses: {
+    type: Array,
+  },
+  /** 加载loading样式 */
+  loadingProps: {
+    type: Object,
+  },
+  /** 提示语 */
+  loadingTexts: {
+    type: Array,
+    value: [],
+  },
+  /** 最大下拉高度 */
+  maxBarHeight: {
+    type: Number,
+    value: 272,
+  },
+  /** 加载中下拉高度 */
+  normalBarHeight: {
+    type: Number,
+    value: 200,
+  },
+  /** 刷新超时时间 */
+  refreshTimeout: {
+    type: Number,
+    value: 3000,
+  },
+};
+
+export default props;
