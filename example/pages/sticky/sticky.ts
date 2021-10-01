ccc7a9ac06ec01b8c4b9625047b226a6b7c78cbe
xihangzhou <xihang_zhou@163.com>
2021-10-01 11:28:11 +0800

    fix:更新sticky demo展示

--- example/pages/sticky/sticky.ts
+++ example/pages/sticky/sticky.ts
@@ -1,12 +1,9 @@
 Page({
   data: {
-    contentRef: null,
     container: null,
   },
   onLoad() {
     this.setData({
-      // @ts-ignore
-      contentRef: () => this.createSelectorQuery().select('.content'),
       // @ts-ignore
       container: () => wx.createSelectorQuery().select('.box-c'),
     });
