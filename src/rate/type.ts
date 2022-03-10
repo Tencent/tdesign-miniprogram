d2b8e095f415418ed361f66537f0160fbd495e59
zhenzhencai <caizhenzhen94@outlook.com>
2022-03-10 15:16:56 +0800

    fix: fix: 修复rate组件在ios真机颜色设置失效的问题

--- src/rate/type.ts
+++ src/rate/type.ts
@@ -17,7 +17,7 @@ export interface TdRateProps {
    */
   color?: {
     type: StringConstructor;
-    optionalTypes: Array<StringConstructor | ArrayConstructor>;
+    optionalTypes: Array<StringConstructor> | StringConstructor;
     value?: string | Array<string>;
   };
   /**
