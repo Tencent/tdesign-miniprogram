4fac17a530bbef655e97b1560a751e4c0bb52fb3
ericjyang <jjy_yang@163.com>
2022-03-18 11:25:26 +0800

    feat: 根据获取的组件info.json信息生成_wechatide.ib.json文件

--- script/gulpfile.wechatide.js
+++ script/gulpfile.wechatide.js
@@ -3,7 +3,6 @@
 const gulp = require('gulp');
 const path = require('path');
 const fs = require('fs');
-const replace = require('gulp-replace');
 
 const wechatideConfig = {
   components: [],
@@ -15,60 +14,30 @@ const wechatideConfig = {
 };
 
 const wechatideFolder = path.join(__dirname, '../_wechatide');
-// 预处理，将miniprogram_npm中的props.js的es module转换commonjs导出存在_wechatide中，方便后续用require引入
-gulp.task('wechatide:pre', (cb) => {
-  gulp
-    .src(['miniprogram_dist/**/props.js'])
-    .pipe(replace('export default props', 'module.exports = props'))
-    .pipe(gulp.dest(wechatideFolder));
-
-  cb();
-});
 
 function isExistFile(path) {
   return fs.existsSync(path);
 }
 
 gulp.task('wechatide:components', (cb) => {
-  // 读取miniprogram_dist而不读取src是因为miniprogram_dist中ts文件已经转化成js文件。
-  const base = path.join(__dirname, '../miniprogram_dist');
-  const src = path.join(__dirname, '../src');
-
-  const componentsFolder = fs.readdirSync(src);
+  const componentsFolder = fs.readdirSync(wechatideFolder);
 
   componentsFolder.forEach((componentName) => {
-    // 获取组件key的值
-    const component = {
-      key: `t-${componentName}`,
-      icon: '',
-      label: '',
-      properties: [],
-      require: {},
-    };
+    const cmpInfoPath = `${wechatideFolder}/${componentName}`;
+    const cmpInfo = fs.readFileSync(cmpInfoPath);
+    const cmpInfoJson = JSON.parse(cmpInfo.toString());
+    wechatideConfig.components.push(cmpInfoJson);
 
-    // 处理props.js 获取properties
-    const componentPropsFilePath = `${wechatideFolder}/${componentName}/props.js`;
-    if (isExistFile(componentPropsFilePath)) {
-      // eslint-disable-next-line global-require
-      const componentProps = require(componentPropsFilePath);
-      Object.keys(componentProps).forEach((key) => {
-        const property = {};
-        property.key = key;
-        property.defaultValue = componentProps[key].value;
-        component.properties.push(property);
-      });
-    }
+    // Todo 处理组件json文件 获取依赖
+    // const componentJsonFilePath = `${base}/${componentName}/${componentName}.json`;
+    // if (isExistFile(componentJsonFilePath)) {
+    //   const componentJsonFile = fs.readFileSync(componentJsonFilePath);
+    //   const componentJson = JSON.parse(componentJsonFile.toString());
+    //   // console.log(componentJson.usingComponents);
+    //   component.require = componentJson.usingComponents;
+    // }
 
-    // 处理组件json文件
-    const componentJsonFilePath = `${base}/${componentName}/${componentName}.json`;
-    if (isExistFile(componentJsonFilePath)) {
-      const componentJsonFile = fs.readFileSync(componentJsonFilePath);
-      const componentJson = JSON.parse(componentJsonFile.toString());
-      // console.log(componentJson.usingComponents);
-      component.require = componentJson.usingComponents;
-    }
-
-    wechatideConfig.components.push(component);
+    // wechatideConfig.components.push(component);
     // console.log(component);
   });
 
@@ -91,7 +60,6 @@ gulp.task('wechatide:generate', (cb) => {
 });
 
 const generate = gulp.series(
-  'wechatide:pre',
   gulp.parallel('wechatide:common', 'wechatide:components', 'wechatide:menu'),
   'wechatide:generate',
 );
