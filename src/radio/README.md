aa78a908bb6c2be3f3734b4107f01f10876a6788
leejimqiu <leejimqiu@gmail.com>
2022-04-07 11:19:25 +0800

    fix(radio): resolve uncontrol usage & update document

--- src/radio/README.md
+++ src/radio/README.md
@@ -18,7 +18,31 @@ isComponent: true
 
 ## 代码演示
 
-### 基础单选框
+### 单个单选框
+
+```html
+<!-- 非受控用法 -->
+<t-radio label="单选" />
+<t-radio defaultChecked="{{true}}" label="单选" />
+
+<!-- 受控用法 -->
+<t-radio checked="{{checked}}" bind:change="handleChange" label="单选" />
+```
+
+```js
+Page({
+  data: {
+    checked: false,
+  },
+  handleChange(e) {
+    this.setData({
+      checked: e.detail.checked,
+    });
+  },
+})
+```
+
+### 组合单选框
 
 <img src="https://tdesign.gtimg.com/miniprogram/readme/radio.png" width="375px" height="50%">
 
@@ -26,13 +50,8 @@ isComponent: true
 <t-radio-group bind:change="onChange" defaultValue="radio1">
   <t-radio value="radio1" label="单选" />
   <t-radio value="radio2" label="单选" />
-  <t-radio
-    value="radio3"
-    label="单选单选单选单选单选单选单选单选单选单选单选单选单选单选"
-  ></t-radio>
-  <t-radio value="radio4" label="单选">
-    单选单选单选单选单选单选单选单选单选单选单选单选单选单选
-  </t-radio>
+  <t-radio value="radio3" label="单选" />
+  <t-radio value="radio4" label="单选" />
 </t-radio-group>
 ```
 
@@ -87,18 +106,20 @@ Page({
 名称 | 类型 | 默认值 | 说明 | 必传
 -- | -- | -- | -- | --
 align | String | left | 复选框和内容相对位置。可选项：left/right | N
-checked | Boolean | - | 是否选中 | N
+allow-uncheck | Boolean | false | 【开发中】是否允许取消选中 | N
+checked | Boolean | false | 是否选中 | N
+default-checked | Boolean | undefined | 是否选中。非受控属性 | N
 color | String | #0052d9 | 单选按钮颜色 | N
 content | String / Slot | - | 单选内容 | N
-content-disabled | Boolean | - | 是否禁用组件内容（content）触发选中 | N
+content-disabled | Boolean | false | 是否禁用组件内容（content）触发选中 | N
 disabled | Boolean | undefined | 是否为禁用态 | N
-external-classes | Array | - | 组件类名，分别用于设置 组件外层、单选图标、主文案、内容 等元素类名。`['t-class', 't-class-icon', 't-class-label', 't-class-content','t-class-border']` | N
+external-classes | Array | - | 组件类名，分别用于设置 组件外层、单选图标、主文案、内容 等元素类名。`['t-class', 't-class-icon', 't-class-label', 't-class-content', 't-class-border']` | N
 icon | String / Array | 'fill-circle' | 自定义选中图标和非选中图标。示例：[选中态图标，非选中态图标]。值为 fill-circle 表示图标为填充型图标，值为 stroke-line 表示图标为描边型图标。TS 类型：`'fill-circle' | 'stroke-line' | Array<string>` | N
 label | String / Slot | - | 主文案 | N
 max-content-row | Number | 5 | 内容最大行数限制 | N
 max-label-row | Number | 3 | 主文案最大行数限制 | N
 name | String | - | HTML 元素原生属性 | N
-value | String / Number / Boolean | undefined | 单选按钮的值。TS 类型：`RadioValue`。[详细类型定义](https://github.com/Tencent/tdesign-miniprogram/tree/develop/src/radio/type.ts) | N
+value | String / Number / Boolean | false | 单选按钮的值。TS 类型：`RadioValue` `type RadioValue = string | number | boolean`。[详细类型定义](https://github.com/Tencent/tdesign-miniprogram/tree/develop/src/radio/type.ts) | N
 
 ### Radio Events
 
@@ -112,8 +133,9 @@ change | `(checked: boolean)` | 值变化时触发
 -- | -- | -- | -- | --
 disabled | Boolean | undefined | 是否禁用全部子单选框 | N
 name | String | - | HTML 元素原生属性 | N
-options | Array | - | 单选组件按钮形式。RadioOption 数据类型为 string 或 number 时，表示 label 和 value 值相同。TS 类型：`Array<RadioOption>`。[详细类型定义](https://github.com/Tencent/tdesign-miniprogram/tree/develop/src/radio/type.ts) | N
-value | String / Number / Boolean | undefined | 选中的值。TS 类型：`RadioValue` | N
+options | Array | - | 单选组件按钮形式。RadioOption 数据类型为 string 或 number 时，表示 label 和 value 值相同。TS 类型：`Array<RadioOption>` `type RadioOption = string | number | RadioOptionObj` `interface RadioOptionObj { label?: string; value?: string | number; disabled?: boolean }`。[详细类型定义](https://github.com/Tencent/tdesign-miniprogram/tree/develop/src/radio/type.ts) | N
+value | String / Number / Boolean | false | 选中的值。TS 类型：`RadioValue` | N
+default-value | String / Number / Boolean | undefined | 选中的值。非受控属性。TS 类型：`RadioValue` | N
 
 ### RadioGroup Events
 
--- src/radio/type.ts
+++ src/radio/type.ts
@@ -12,23 +12,30 @@ export interface TdRadioProps {
   align?: {
     type: StringConstructor;
     value?: 'left' | 'right';
-    required?: boolean;
+  };
+  /**
+   * 是否允许取消选中
+   * @default false
+   */
+  allowUncheck?: {
+    type: BooleanConstructor;
+    value?: boolean;
   };
   /**
    * 是否选中
+   * @default false
    */
   checked?: {
     type: BooleanConstructor;
     value?: boolean;
-    required?: boolean;
   };
   /**
    * 是否选中，非受控属性
+   * @default false
    */
   defaultChecked?: {
     type: BooleanConstructor;
     value?: boolean;
-    required?: boolean;
   };
   /**
    * 单选按钮颜色
@@ -37,7 +44,6 @@ export interface TdRadioProps {
   color?: {
     type: StringConstructor;
     value?: string;
-    required?: boolean;
   };
   /**
    * 单选内容
@@ -45,15 +51,14 @@ export interface TdRadioProps {
   content?: {
     type: StringConstructor;
     value?: string;
-    required?: boolean;
   };
   /**
    * 是否禁用组件内容（content）触发选中
+   * @default false
    */
   contentDisabled?: {
     type: BooleanConstructor;
     value?: boolean;
-    required?: boolean;
   };
   /**
    * 是否为禁用态
@@ -61,15 +66,13 @@ export interface TdRadioProps {
   disabled?: {
     type: BooleanConstructor;
     value?: boolean;
-    required?: boolean;
   };
   /**
    * 组件类名，分别用于设置 组件外层、单选图标、主文案、内容 等元素类名
    */
   externalClasses?: {
     type: ArrayConstructor;
-    value?: ['t-class', 't-class-icon', 't-class-label', 't-class-content'];
-    required?: boolean;
+    value?: ['t-class', 't-class-icon', 't-class-label', 't-class-content', 't-class-border'];
   };
   /**
    * 自定义选中图标和非选中图标。示例：[选中态图标，非选中态图标]。值为 fill-circle 表示图标为填充型图标，值为 stroke-line 表示图标为描边型图标
@@ -79,7 +82,6 @@ export interface TdRadioProps {
     type: StringConstructor;
     optionalTypes: Array<ArrayConstructor>;
     value?: 'fill-circle' | 'stroke-line' | Array<string>;
-    required?: boolean;
   };
   /**
    * 主文案
@@ -87,7 +89,6 @@ export interface TdRadioProps {
   label?: {
     type: StringConstructor;
     value?: string;
-    required?: boolean;
   };
   /**
    * 内容最大行数限制
@@ -96,7 +97,6 @@ export interface TdRadioProps {
   maxContentRow?: {
     type: NumberConstructor;
     value?: number;
-    required?: boolean;
   };
   /**
    * 主文案最大行数限制
@@ -105,7 +105,6 @@ export interface TdRadioProps {
   maxLabelRow?: {
     type: NumberConstructor;
     value?: number;
-    required?: boolean;
   };
   /**
    * HTML 元素原生属性
@@ -114,16 +113,15 @@ export interface TdRadioProps {
   name?: {
     type: StringConstructor;
     value?: string;
-    required?: boolean;
   };
   /**
    * 单选按钮的值
+   * @default false
    */
   value?: {
     type: StringConstructor;
     optionalTypes: Array<NumberConstructor | BooleanConstructor>;
     value?: RadioValue;
-    required?: boolean;
   };
 }
 
@@ -134,7 +132,6 @@ export interface TdRadioGroupProps {
   disabled?: {
     type: BooleanConstructor;
     value?: boolean;
-    required?: boolean;
   };
   /**
    * HTML 元素原生属性
@@ -143,7 +140,6 @@ export interface TdRadioGroupProps {
   name?: {
     type: StringConstructor;
     value?: string;
-    required?: boolean;
   };
   /**
    * 单选组件按钮形式。RadioOption 数据类型为 string 或 number 时，表示 label 和 value 值相同
@@ -151,24 +147,24 @@ export interface TdRadioGroupProps {
   options?: {
     type: ArrayConstructor;
     value?: Array<RadioOption>;
-    required?: boolean;
   };
   /**
    * 选中的值
+   * @default false
    */
   value?: {
     type: StringConstructor;
     optionalTypes: Array<NumberConstructor | BooleanConstructor>;
     value?: RadioValue;
-    required?: boolean;
   };
   /**
    * 选中的值，非受控属性
+   * @default false
    */
   defaultValue?: {
     type: StringConstructor;
+    optionalTypes: Array<NumberConstructor | BooleanConstructor>;
     value?: RadioValue;
-    required?: boolean;
   };
 }
 
