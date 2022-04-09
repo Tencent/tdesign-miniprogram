2f6f36832d6f98dc8ed60146abe1fa5c04ba9241
leejim <leejimqiu@gmail.com>
2022-04-09 23:24:42 +0800

    fix(badge): export standard type

--- src/tab-bar/type.ts
+++ src/tab-bar/type.ts
@@ -4,7 +4,7 @@
  * 该文件为脚本自动生成文件，请勿随意修改。如需修改请联系 PMC
  * */
 
-import { TdBadgeProps } from '../badge/index';
+import { BadgeProps } from '../badge/index';
 
 export interface TdTabBarProps {
   /**
@@ -78,7 +78,8 @@ export interface TdTabBarItemProps {
    */
   badgeProps?: {
     type: ObjectConstructor;
-    value?: TdBadgeProps;
+    value?: BadgeProps;
+    required?: boolean;
   };
   /**
    * 图标名称
