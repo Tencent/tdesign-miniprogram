cb18e0af47df40d25210a289998cac3b3fc10051
310227663@qq.com <twinkleli@tencent.com>
2022-04-24 11:45:56 +0800

    feat: modify gulpfile.  copy .md to __example folder

--- script/gulpfile.base.js
+++ script/gulpfile.base.js
@@ -46,7 +46,6 @@ module.exports = (src, dist, moduleName) => {
   const srcOptions = { base: src, ignore: ['**/__test__', '**/__test__/**'] };
   const watchOptions = { events: ['add', 'change'] };
   const gulpErrorPath = 'example/utils/gulpError.js';
-
   // 文件匹配路径
   const globs = {
     ts: `${src}/**/*.ts`, // 匹配 ts 文件
@@ -66,7 +65,6 @@ module.exports = (src, dist, moduleName) => {
     `!${globs.json}`,
     `!${globs.less}`,
     `!${globs.wxss}`,
-    `!${globs.md}`,
   ];
 
   // 包装 gulp.lastRun, 引入文件 ctime 作为文件变动判断另一标准
