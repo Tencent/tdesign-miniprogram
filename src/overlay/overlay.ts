996cce5e61c132da34322cd6f05743c1d73a48b9
leejimqiu <leejimqiu@gmail.com>
2022-04-28 11:32:45 +0800

    feat(overlay): enrich properties

--- src/overlay/overlay.ts
+++ src/overlay/overlay.ts
@@ -21,16 +21,36 @@ export default class Overlay extends SuperComponent {
       type: Number,
       value: 300,
     },
+
+    backgroundColor: {
+      type: String,
+      value: '',
+    },
+
+    preventScrollThrough: {
+      type: Boolean,
+      value: true,
+    },
   };
 
   data = {
     prefix,
     classPrefix: name,
+    computedStyle: '',
+  };
+
+  observers = {
+    backgroundColor(v) {
+      this.setData({
+        computedStyle: `; background-color: ${v}`,
+      });
+    },
   };
 
   methods = {
     handleClick() {
       this.triggerEvent('click', { visible: !this.properties.visible });
     },
+    noop() {},
   };
 }
