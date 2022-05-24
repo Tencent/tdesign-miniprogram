577cbc187f62945e0b1888cc4b232ffe8ec552fa
Peter Qiu <273743552@qq.com>
2022-05-24 23:36:10 +0800

    fix(dialog): resolve open-type button correctly

--- src/dialog/props.ts
+++ src/dialog/props.ts
@@ -28,7 +28,7 @@ const props: TdDialogProps = {
   },
   /** 确认按钮。值为 null 则不显示确认按钮。值类型为字符串，则表示自定义按钮文本，值类型为 Object 则表示透传 Button 组件属性。使用 TNode 自定义按钮时，需自行控制确认事件 */
   confirmBtn: {
-    type: String,
+    type: null,
     optionalTypes: [Object],
     value: '',
   },
