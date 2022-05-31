6cff55f6b291693fd50b7d5d533ce33c2da8b49f
leejimqiu <leejimqiu@gmail.com>
2022-05-31 11:31:31 +0800

    fix(collapse): correct external class

--- src/collapse/README.md
+++ src/collapse/README.md
@@ -77,6 +77,7 @@ change | `(value: CollapseValue)` | 切换面板时触发，返回变化的值
 content | String / Slot | - | 折叠面板内容 | N
 disabled | Boolean | undefined | 禁止当前面板展开，优先级大于 Collapse 的同名属性 | N
 expand-icon | Boolean / Slot | undefined | 当前折叠面板展开图标，优先级大于 Collapse 的同名属性 | N
+external-classes | Array | - | 组件类名，用于组件外层元素、标题、内容。`['t-class', 't-class-header', 't-class-content']` | N
 header | String / Slot | - | 面板头内容 | N
 header-right-content | String / Slot | - | 面板头的右侧区域，一般用于呈现面板操作 | N
 value | String / Number | - | 当前面板唯一标识，如果值为空则取当前面下标兜底作为唯一标识 | N
--- src/collapse/collapse-panel.ts
+++ src/collapse/collapse-panel.ts
@@ -11,7 +11,7 @@ const nextTick = () => new Promise((resolve) => setTimeout(resolve, 20));
 export interface CollapsePanelProps extends TdCollapsePanelProps {}
 @wxComponent()
 export default class CollapsePanel extends SuperComponent {
-  externalClasses = [`${prefix}-class`];
+  externalClasses = [`${prefix}-class`, `${prefix}-class-content`, `${prefix}-class-header`];
 
   options = {
     multipleSlots: true,
@@ -37,6 +37,7 @@ export default class CollapsePanel extends SuperComponent {
   properties = props;
 
   data = {
+    prefix,
     contentHeight: 0,
     expanded: false,
     classPrefix: name,
