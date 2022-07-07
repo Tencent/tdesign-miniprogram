66f11813082ddd75ef1fae3e68d9945240f894da
anlyyao <anly_yaw@163.com>
2022-07-07 23:29:24 +0800

    fix(tag): complete components && update styles

--- src/tag/check-tag.ts
+++ src/tag/check-tag.ts
@@ -1,6 +1,7 @@
 import { wxComponent, SuperComponent } from '../common/src/index';
 import config from '../common/config';
 import props from './check-tag-props';
+import { classNames } from '../common/utils';
 
 const { prefix } = config;
 const name = `${prefix}-tag`;
@@ -8,8 +9,9 @@ const name = `${prefix}-tag`;
 @wxComponent()
 export default class CheckTag extends SuperComponent {
   data = {
+    prefix,
     classPrefix: name,
-    classBasePrefix: prefix,
+    className: '',
   };
 
   properties = props;
@@ -21,15 +23,56 @@ export default class CheckTag extends SuperComponent {
     },
   ];
 
+  options = {
+    multipleSlots: true,
+  };
+
+  lifetimes = {
+    attached() {
+      this.setClass();
+    },
+  };
+
+  observers = {
+    checked() {
+      this.setClass();
+    },
+  };
+
   methods = {
-    onClickClose(e: WechatMiniprogram.BaseEvent) {
+    setClass() {
+      const { prefix, classPrefix } = this.data;
+      const { size, shape, closable, disabled, checked, defaultChecked } = this.properties;
+      const isChecked = checked || defaultChecked;
+      const tagClass = [
+        classPrefix,
+        `${classPrefix}--checkable`,
+        closable ? `${classPrefix}--closable ${prefix}-is-closable` : '',
+        disabled ? `${classPrefix}--disabled ${prefix}-is-disabled` : '',
+        isChecked ? `${classPrefix}--checked ${prefix}-is-checked` : '',
+        `${classPrefix}--size-${size || 'medium'}`,
+        `${classPrefix}--shape-${shape || 'square'}`,
+      ];
+      const className = classNames(tagClass);
+      this.setData({
+        className,
+      });
+    },
+
+    hangleClose(e: WechatMiniprogram.BaseEvent) {
+      if (this.data.disabled) return;
+
       this.triggerEvent('close', e);
     },
 
     handleChange() {
       if (this.data.disabled) return;
-
-      this._trigger('change', { checked: !this.data.checked });
+      const { checked, defaultChecked } = this.properties;
+      const isChecked = checked || defaultChecked;
+      this.setData({
+        checked: !isChecked,
+      });
+      this._trigger('change', { checked: !isChecked });
     },
   };
 }
--- src/tag/tag.ts
+++ src/tag/tag.ts
@@ -1,6 +1,7 @@
 import { wxComponent, SuperComponent } from '../common/src/index';
 import config from '../common/config';
 import props from './props';
+import { classNames } from '../common/utils';
 
 const { prefix } = config;
 const name = `${prefix}-tag`;
@@ -8,16 +9,51 @@ const name = `${prefix}-tag`;
 @wxComponent()
 export default class Tag extends SuperComponent {
   data = {
+    prefix,
     classPrefix: name,
-    classBasePrefix: prefix,
+    className: '',
   };
-
-  externalClasses = [`${prefix}-class`];
 
   properties = props;
 
+  externalClasses = [`${prefix}-class`];
+
+  options = {
+    multipleSlots: true,
+  };
+
+  lifetimes = {
+    attached() {
+      this.setClass();
+    },
+  };
+
   methods = {
+    setClass() {
+      const { prefix, classPrefix } = this.data;
+      const { size, shape, theme, variant, closable, disabled } = this.properties;
+      const tagClass = [
+        classPrefix,
+        `${classPrefix}--theme-${theme || 'default'}`,
+        `${classPrefix}--variant-${variant || 'dark'}`,
+        closable ? `${classPrefix}--closable ${prefix}-is-closable` : '',
+        disabled ? `${classPrefix}--disabled ${prefix}-is-disabled` : '',
+        `${classPrefix}--size-${size || 'medium'}`,
+        `${classPrefix}--shape-${shape || 'square'}`,
+      ];
+      const className = classNames(tagClass);
+      this.setData({
+        className,
+      });
+    },
+
+    handleClick(e: WechatMiniprogram.BaseEvent) {
+      if (this.data.disabled) return;
+      this.triggerEvent('click', e);
+    },
+
     hangleClose(e: WechatMiniprogram.BaseEvent) {
+      if (this.data.disabled) return;
       this.triggerEvent('close', e);
     },
   };
