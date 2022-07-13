3d4450c5df879311a473af3ae56af812e30f6ba2
leejimqiu <leejimqiu@gmail.com>
2022-07-13 15:21:46 +0800

    refactor(popup): upgrade demo

--- src/popup/_example/popup.ts
+++ src/popup/_example/popup.ts
@@ -1,64 +1,30 @@
 Page({
+  placement: '',
+
   data: {
     bottom: false,
-    top: false,
     center: false,
     left: false,
     right: false,
-    operList: [
-      {
-        title: '弹出层出现为止可能为顶部、底部、中部、左侧或右侧',
-        btns: [
-          {
-            type: 'top',
-            text: '顶部弹出',
-          },
-          {
-            type: 'bottom',
-            text: '底部弹出',
-          },
-          {
-            type: 'center',
-            text: '中部弹出',
-          },
-          {
-            type: 'left',
-            text: '左侧弹出',
-          },
-          {
-            type: 'right',
-            text: '右侧弹出',
-          },
-        ],
-      },
-    ],
   },
 
-  clickHandle({ detail: placement }) {
+  handlePopup(e) {
+    const placement = e.currentTarget.dataset.type;
+
+    this.placement = placement;
+
     this.setData({
-      [`${placement}`]: true,
-    });
-  },
-  onTriggerClick() {
-    this.setData({
-      visible: !this.data.visible,
+      [placement]: true,
     });
   },
 
   onVisibleChange({ detail }) {
     const { visible } = detail;
-    this.setData({
-      top: visible,
-      bottom: visible,
-      center: visible,
-      left: visible,
-      right: visible,
-    });
-  },
 
-  onClose() {
-    this.setData({
-      visible: false,
-    });
+    if (this.placement) {
+      this.setData({
+        [this.placement]: visible,
+      });
+    }
   },
 });
