83ef39ef1325a3d6c8ab946ae583e83a19d18cc6
anlyyao <anly_yaw@163.com>
2022-07-27 09:58:09 +0800

    docs(swiper): update code sample

---
+++ src/swiper/_example/base/index.js
@@ -0,0 +1,37 @@
+const imageCdn = 'https://tdesign.gtimg.com/site/swiper';
+const swiperList = [
+  {
+    image: `${imageCdn}/01.png`,
+  },
+  {
+    image: `${imageCdn}/02.png`,
+  },
+  {
+    image: `${imageCdn}/03.png`,
+  },
+  {
+    image: `${imageCdn}/04.png`,
+  },
+  {
+    image: `${imageCdn}/05.png`,
+  },
+];
+
+Component({
+  data: {
+    current: 1,
+    autoplay: true,
+    duration: 500,
+    interval: 5000,
+    swiperList,
+  },
+
+  methods: {
+    onChange(e) {
+      const {
+        detail: { current, source },
+      } = e;
+      console.log(current, source);
+    },
+  },
+});
