37d6226f447b1aa56b2f34257e9c6966a42800d7
leejimqiu <leejimqiu@gmail.com>
2022-08-03 14:37:27 +0800

    fix(checkbox): update demo

--- src/checkbox/_example/type/index.js
+++ src/checkbox/_example/type/index.js
@@ -4,6 +4,8 @@ Component({
    */
   data: {
     demoCheckbox2: ['checkbox2', 'checkbox3'],
+    activeImage: 'https://oteam-tdesign-1258344706.cos.ap-guangzhou.myqcloud.com/miniprogram/checkbox-checked.png',
+    inActiveImage: 'https://oteam-tdesign-1258344706.cos.ap-guangzhou.myqcloud.com/miniprogram/checkbox.png',
   },
 
   /**
