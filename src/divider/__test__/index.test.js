5df5c68378d3867c25cda11839241a723d284b39
oliverppeng <oliverppeng@tencent.com>
2022-08-16 17:36:40 +0800

    test(divider): add unit test

--- src/divider/__test__/index.test.js
+++ src/divider/__test__/index.test.js
@@ -4,9 +4,6 @@ import path from 'path';
 // 文本位置
 const textAlign = ['left', 'center', 'right'];
 
-// 内容形式
-const content = ['string', 'slot'];
-
 describe('Divider', () => {
   const divider = simulate.load(path.resolve(__dirname, `../divider`), 't-divider', {
     less: true,
@@ -16,38 +13,36 @@ describe('Divider', () => {
   textAlign.forEach((align) => {
     it(`:align ${align} render correctly`, () => {
       const id = simulate.load({
-        template: `<t-divider></t-divider>`,
+        template: `<t-divider class="divider-align"></t-divider>`,
+        data: {
+          content: '文字信息',
+          align,
+        },
         usingComponents: {
           't-divider': divider,
         },
       });
       const comp = simulate.render(id);
-      comp.setData({
-        content: '文字信息',
-        align,
-      });
       comp.attach(document.createElement('parent-wrapper'));
 
       expect(comp.toJSON()).toMatchSnapshot();
     });
   });
 
-  content.forEach((content) => {
-    it(`:content ${content} render correctly`, () => {
-      const id = simulate.load({
-        template: `<t-divider></t-divider>`,
-        usingComponents: {
-          't-divider': divider,
-        },
-      });
-      const comp = simulate.render(id);
-      comp.setData({
-        content: '文字信息',
-      });
-      comp.attach(document.createElement('parent-wrapper'));
-
-      expect(comp.toJSON()).toMatchSnapshot();
+  it(`:content render correctly`, () => {
+    const id = simulate.load({
+      template: `<t-divider></t-divider>`,
+      usingComponents: {
+        't-divider': divider,
+      },
     });
+    const comp = simulate.render(id);
+    comp.setData({
+      content: '文字信息',
+    });
+    comp.attach(document.createElement('parent-wrapper'));
+
+    expect(comp.toJSON()).toMatchSnapshot();
   });
 
   it(':dashed render correctly', () => {
