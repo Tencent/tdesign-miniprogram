8ec0dc9eacc9e7f29092eac79b2430c1e66d6456
sam <isanxia@qq.com>
2022-08-23 22:53:45 +0800

    test(steps): add unit test

---
+++ src/steps/__test__/index.js
@@ -0,0 +1,38 @@
+Component({
+  data: {
+    current1: 0,
+    current2: 0,
+    layout: 'horizontal',
+    readonly: false,
+    theme: 'default',
+    currentSub: '1-0',
+    subStepItems1: [
+      {
+        title: '二级步骤描述',
+        status: 'process',
+      },
+      {
+        title: '二级步骤描述',
+        status: 'finish',
+      },
+    ],
+    subStepItems2: [
+      {
+        title: '二级步骤描述',
+        status: 'process',
+      },
+      {
+        title: '二级步骤描述',
+        status: 'error',
+      },
+    ],
+  },
+  methods: {
+    onChange1(e) {
+      this.setData({ current1: e.detail.current });
+    },
+    onChange2(e) {
+      this.setData({ current2: e.detail.current });
+    },
+  },
+});
