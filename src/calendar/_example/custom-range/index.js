1cff3b6b7898f181c9c9a2ab5b176e8373397a17
leejimqiu <leejimqiu@gmail.com>
2022-09-30 19:38:07 +0800

    feat(calendar): add demo

--- src/calendar/_example/custom-range/index.js
+++ src/calendar/_example/custom-range/index.js
@@ -1,6 +1,9 @@
 Component({
   data: {
     visible: false,
+    value: new Date(2022, 1, 15).getTime(),
+    minDate: new Date(2022, 1, 1).getTime(),
+    maxDate: new Date(2022, 1, 27).getTime(),
   },
   methods: {
     handleCalendar() {
