9ba5adc1960cd544a0ed230c17dbef202ef3a63f
leejimqiu <leejimqiu@gmail.com>
2022-11-13 16:56:46 +0800

    fix(grid): remove virtual-host option

--- src/grid/grid-item.ts
+++ src/grid/grid-item.ts
@@ -23,7 +23,7 @@ export default class GridItem extends SuperComponent {
 
   options = {
     multipleSlots: true,
-    virtualHost: true,
+    // virtualHost: true,
   };
 
   relations: RelationsOptions = {
