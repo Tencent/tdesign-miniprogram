540bb35db2c681522c6e3c5d38ec57a062b2829e
Isabella327 <liangchong327@163.com>
2022-12-06 20:43:08 +0800

    fix: demo

--- src/tab-bar/_example/icon-only/index.js
+++ src/tab-bar/_example/icon-only/index.js
@@ -3,7 +3,7 @@ Component({
     value: 'label_1',
     list: [
       { value: 'label_1', icon: 'home', ariaLabel: '首页' },
-      { value: 'label_2', icon: 'app', ariaLabel: 'app' },
+      { value: 'label_2', icon: 'app', ariaLabel: '软件' },
       { value: 'label_3', icon: 'chat', ariaLabel: '聊天' },
       { value: 'label_4', icon: 'user', ariaLabel: '我的' },
     ],
