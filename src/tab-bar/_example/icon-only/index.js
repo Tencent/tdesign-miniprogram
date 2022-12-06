5abac221f983a04d02417fc7af2958644804795a
Isabella327 <liangchong327@163.com>
2022-12-06 16:25:16 +0800

    feat: feat:aira-tabbar

--- src/tab-bar/_example/icon-only/index.js
+++ src/tab-bar/_example/icon-only/index.js
@@ -2,10 +2,10 @@ Component({
   data: {
     value: 'label_1',
     list: [
-      { value: 'label_1', icon: 'app' },
+      { value: 'label_1', icon: 'home', ariaLabel: '首页' },
       { value: 'label_2', icon: 'app' },
-      { value: 'label_3', icon: 'app' },
-      { value: 'label_4', icon: 'app' },
+      { value: 'label_3', icon: 'chat' },
+      { value: 'label_4', icon: 'user' },
     ],
   },
 
