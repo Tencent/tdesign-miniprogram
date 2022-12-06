72fe18089839a40317e04ed4f7619391e07d1058
Isabella327 <liangchong327@163.com>
2022-12-06 19:30:53 +0800

    fix: aria-tabbar

--- src/tab-bar/_example/icon-only/index.js
+++ src/tab-bar/_example/icon-only/index.js
@@ -3,9 +3,9 @@ Component({
     value: 'label_1',
     list: [
       { value: 'label_1', icon: 'home', ariaLabel: '首页' },
-      { value: 'label_2', icon: 'app' },
-      { value: 'label_3', icon: 'chat' },
-      { value: 'label_4', icon: 'user' },
+      { value: 'label_2', icon: 'app', ariaLabel: 'app' },
+      { value: 'label_3', icon: 'chat', ariaLabel: '聊天' },
+      { value: 'label_4', icon: 'user', ariaLabel: '我的' },
     ],
   },
 
