20936ab002e9e5c9c9466f181dafc038a6d1178f
zhangpaopao <zhangpaopao0609@gmail.com>
2022-12-08 10:51:19 +0800

    feat(grid): update useId(remove blur)

--- src/common/src/useId.ts
+++ src/common/src/useId.ts
@@ -1,6 +1,10 @@
+import config from '../config';
+
+const { prefix } = config;
+
 const defaultId = {
-  blur: Math.floor(Math.random() * 100),
+  // blur: Math.floor(Math.random() * 100), 保留 blur 功能，用于混淆 id 避免 id 重复，先不使用，如有必要再添加
   current: 0,
 };
 
-export const useId = (prefix: string): string => `${prefix}-id-${defaultId.blur}-${defaultId.current++}`;
+export const useId = (): string => `${prefix}-id-${defaultId.current++}`;
--- src/grid/grid-item.ts
+++ src/grid/grid-item.ts
@@ -56,7 +56,7 @@ export default class GridItem extends SuperComponent {
   lifetimes = {
     ready() {
       this.setData({
-        labelId: useId(name),
+        labelId: useId(),
       });
     },
   };
