a43decfd5e01208e96b2e044fb502c8f68cd1688
leejimqiu <leejimqiu@gmail.com>
2022-12-23 23:44:50 +0800

    test: add demo test

---
+++ src/tree-select/__test__/demo.test.js
@@ -0,0 +1,19 @@
+/**
+ * 该文件为由脚本 `npm run test:demo` 自动生成，如需修改，执行脚本命令即可。请勿手写直接修改，否则会被覆盖
+ */
+
+import simulate from 'miniprogram-simulate';
+import path from 'path';
+
+const mapper = ['base', 'multiple', 'normal'];
+
+describe('TreeSelect', () => {
+  mapper.forEach((demoName) => {
+    it(`TreeSelect ${demoName} demo works fine`, () => {
+      const id = load(path.resolve(__dirname, `../../tree-select/_example/${demoName}/index`), demoName);
+      const container = simulate.render(id);
+      container.attach(document.createElement('parent-wrapper'));
+      expect(container.toJSON()).toMatchSnapshot();
+    });
+  });
+});
