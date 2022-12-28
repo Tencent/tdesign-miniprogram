de173d4ed8bf4ad6211552530f23a87929f6d140
anlyyao <anly_yaw@163.com>
2022-12-28 18:07:48 +0800

    style: remove useless css

--- src/empty/_example/imageEmpty/index.js
+++ src/empty/_example/imageEmpty/index.js
@@ -1,5 +1,5 @@
 Component({
   data: {
-    image: 'https://oteam-tdesign-1258344706.cos.ap-guangzhou.myqcloud.com/miniprogram/empty__demo-image.png',
+    image: 'https://oteam-tdesign-1258344706.cos.ap-guangzhou.tencentcos.cn/miniprogram/images/empty-image.png',
   },
 });
