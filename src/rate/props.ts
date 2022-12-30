73aa842743b2e7ce6660a4627136d747112d9c7a
leejimqiu <leejimqiu@gmail.com>
2022-12-30 12:26:27 +0800

    fix(rate): update style (#1350)

--- src/rate/props.ts
+++ src/rate/props.ts
@@ -36,10 +36,10 @@ const props: TdRateProps = {
   },
   /** 评分图标的间距 */
   gap: {
-    type: Number,
-    value: 4,
+    type: null,
+    value: 8,
   },
-  /** 自定义评分图标；此时不受 variant 影响 */
+  /** 自定义评分图标，[选中图标，未选中图标]；此时不受 variant 影响 */
   icon: {
     type: null,
   },
