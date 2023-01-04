079f3ded5b498669ae7ec8fcec16577b3d12f089
leejimqiu <leejimqiu@gmail.com>
2023-01-04 11:31:41 +0800

    fix(swiper): update api (#1387)
    * fix(swiper): update api
    
    * test(swiper): update snapshot

--- src/swiper/swiper-nav.ts
+++ src/swiper/swiper-nav.ts
@@ -4,34 +4,38 @@ import config from '../common/config';
 const { prefix } = config;
 const name = `${prefix}-swiper-nav`;
 
-type NavOptions = {
-  index: number;
-  total: number;
-  direction: boolean;
-  paginationPosition: string;
-};
-
 @wxComponent()
 export default class SwiperNav extends SuperComponent {
   externalClasses = [`${prefix}-class`];
 
   properties = {
-    // 页码导航类型
+    current: {
+      type: Number,
+      value: 0,
+    },
+    total: {
+      type: Number,
+      value: 0,
+    },
     type: {
       type: String,
       value: 'dots',
     },
-    // 最小显示数量，即小于这个数不会显示导航器
     minShowNum: {
       type: Number,
       value: 2,
     },
-    /**
-     * 是否开启导航按钮
-     */
     showControls: {
       type: Boolean,
       value: false,
+    },
+    direction: {
+      type: String,
+      value: 'horizontal',
+    },
+    paginationPosition: {
+      type: String,
+      value: 'bottom',
     },
   };
 
@@ -42,20 +46,11 @@ export default class SwiperNav extends SuperComponent {
   };
 
   data = {
-    index: 0,
-    total: 0,
-    direction: 'horizontal',
     prefix,
     classPrefix: name,
   };
 
   methods = {
-    onChange(opt: NavOptions) {
-      this.setData({
-        ...opt,
-      });
-    },
-
     nav(e) {
       const { dir } = e.target.dataset;
       const source = 'nav';
