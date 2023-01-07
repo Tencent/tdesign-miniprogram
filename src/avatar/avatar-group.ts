cdb757c394837c7078da7a26cb834aa97c5fe8c5
leejimqiu <leejimqiu@gmail.com>
2023-01-07 20:55:23 +0800

    fix(avatar): optimize code (#1441)
    * fix(avatar): optimize code
    
    * test(avatar): update

--- src/avatar/avatar-group.ts
+++ src/avatar/avatar-group.ts
@@ -29,110 +29,57 @@ export default class AvatarGroup extends SuperComponent {
     },
   };
 
-  ready() {
-    this.setData({
-      length: this.$children.length,
-    });
-    this.handleHasChild(this.$children, this.data.hasChild);
-    this.handleChildSlot(this.properties.max, this.$children, this.handleChildMax);
-    this.handleChildSize(this.properties.size, this.$children);
-    this.handleChildCascading(this.properties.cascading, this.$children);
-  }
-
   lifetimes = {
     attached() {
+      this.setClass();
+    },
+    ready() {
+      this.setData({
+        length: this.$children.length,
+      });
+      this.handleMax();
+      this.handleChildCascading();
+    },
+  };
+
+  observers = {
+    'cascading, size'() {
       this.setClass();
     },
   };
 
   methods = {
-    isINcludePX(size) {
-      return size.indexOf('px') > -1;
-    },
-
     setClass() {
       const { cascading, size } = this.properties;
-      const classList = [name, `${prefix}-class`];
       const direction = cascading.split('-')[0];
-      if (this.isINcludePX(size)) {
-        classList.push(`${name}-offset-${direction}-medium`);
-      } else {
-        classList.push(`${name}-offset-${direction}-${size}`);
-      }
+      const classList = [
+        name,
+        `${prefix}-class`,
+        `${name}-offset-${direction}-${size.indexOf('px') > -1 ? 'medium' : size}`,
+      ];
+
       this.setData({
         className: classList.join(' '),
       });
     },
 
-    /**
-     * @param children avatar-group的所有avatar子节点
-     * @param hasChild 是否为子节点（slot插槽以及默认的avatar折叠元素没有parent）
-     * @description
-     */
-    handleHasChild(children, hasChild) {
-      children.forEach((child) => {
-        child.updateIsChild(hasChild);
-      });
-    },
-    /**
-     *
-     * @param max 最大avatar数量
-     * @param children avatar-group的所有avatar子节点
-     * @param f handleChildMax函数
-     * @description 判断是否传入自定义折叠元素
-     */
-    handleChildSlot(max, children, f) {
-      const query = this.createSelectorQuery();
-      const slotName = `.${this.data.classPrefix}__collapse--slot`;
-      query.select(slotName).boundingClientRect();
-      query.exec((res) => {
-        const isSlot = !!res[0].width;
-        f(max, children, isSlot);
-      });
-    },
-
-    /**
-     *
-     * @param max 最大avatar数量
-     * @param children avatar-group的所有avatar子节点
-     * @param isSlotElement 是否传入自定义折叠元素（res[0].width不为空表示传入）
-     * @description avatar-group的最大数量处理
-     *              + 如果未传入自定义折叠元素，当超出max数量时，显示默认折叠元素
-     */
-    handleChildMax(max, children, isSlotElement) {
-      const len = children.length;
+    handleMax() {
+      const { max } = this.data;
+      const len = this.$children.length;
       if (!max || max > len) return;
 
-      const slotElement = isSlotElement ? children.pop() : '';
-      const leftChildren = children.splice(max, len - max, isSlotElement && slotElement);
+      const restAvatars = this.$children.splice(max, len - max);
 
-      leftChildren.forEach((child) => {
-        child.updateShow();
+      restAvatars.forEach((child) => {
+        child.hide();
       });
     },
 
-    /**
-     * @param size 尺寸，优先级低于avatar自身设置的size
-     * @param children avatar-group的所有avatar子节点
-     * @description 更新avatar节点的size属性
-     */
-    handleChildSize(size, children) {
-      if (!size) return;
-      children.forEach((child) => {
-        child.updateSize(size);
-      });
-    },
-
-    /**
-     * @param cascading 层叠关系
-     * @param children avatar-group的所有avatar子节点
-     * @description 右侧图片在上可用css直接实现，左侧图片在上，通过设置z-index实现
-     */
-    handleChildCascading(cascading, children) {
-      if (cascading === 'right-up') return;
+    handleChildCascading() {
+      if (this.properties.cascading === 'right-up') return;
 
       const defaultZIndex = 100;
-      children.forEach((child, index) => {
+      this.$children.forEach((child, index) => {
         child.updateCascading(defaultZIndex - index * 10);
       });
     },
