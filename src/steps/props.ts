dad54cb85bac6e403f5ca3b4cce32376c4d09eb0
leejimqiu <leejimqiu@gmail.com>
2023-01-09 16:05:41 +0800

    refactor(steps): make step-item a separate directory (#1456)

--- src/steps/props.ts
+++ src/steps/props.ts
@@ -6,25 +6,19 @@
 
 import { TdStepsProps } from './type';
 const props: TdStepsProps = {
-  /** 当前步骤，即整个步骤条进度，格式为`1`、`1-0`或`1-1`。默认根据步骤下标判断步骤的完成状态，当前步骤为进行中，当前步骤之前的步骤为已完成，当前步骤之后的步骤为未开始。若当前步骤条存在子步骤条，则会根据子步骤条重新判断当前步骤状态（子步骤条中存在error，则当前步骤error，子步骤条中存在process，当前步骤process，若最后一个子步骤条finish，当前步骤finish，优先级为`finish>error>process`）。注意：如果每个步骤条单独设置了status，则步骤条为设定的status，若传入`status:''`,将默认为未开始状态,传入的status优先级最高。 */
+  /** 当前步骤，即整个步骤条进度。默认根据步骤下标判断步骤的完成状态，当前步骤为进行中，当前步骤之前的步骤为已完成，当前步骤之后的步骤为未开始。如果每个步骤没有设置 value，current 值为步骤长度则表示所有步骤已完成。如果每个步骤设置了自定义 value，则 current = 'FINISH' 表示所有状态完成 */
   current: {
     type: null,
     value: null,
   },
-  /** 当前步骤，即整个步骤条进度，格式为`1`、`1-0`或`1-1`。默认根据步骤下标判断步骤的完成状态，当前步骤为进行中，当前步骤之前的步骤为已完成，当前步骤之后的步骤为未开始。若当前步骤条存在子步骤条，则会根据子步骤条重新判断当前步骤状态（子步骤条中存在error，则当前步骤error，子步骤条中存在process，当前步骤process，若最后一个子步骤条finish，当前步骤finish，优先级为`finish>error>process`）。注意：如果每个步骤条单独设置了status，则步骤条为设定的status，若传入`status:''`,将默认为未开始状态,传入的status优先级最高。，非受控属性 */
+  /** 当前步骤，即整个步骤条进度。默认根据步骤下标判断步骤的完成状态，当前步骤为进行中，当前步骤之前的步骤为已完成，当前步骤之后的步骤为未开始。如果每个步骤没有设置 value，current 值为步骤长度则表示所有步骤已完成。如果每个步骤设置了自定义 value，则 current = 'FINISH' 表示所有状态完成，非受控属性 */
   defaultCurrent: {
     type: null,
-    value: 0,
   },
   /** 用于控制 current 指向的步骤条的状态 */
   currentStatus: {
     type: String,
     value: 'process',
-  },
-  /** 自定义组件样式 */
-  customStyle: {
-    type: String,
-    value: '',
   },
   /** 组件类名，用于设置组件外层元素元素类名 */
   externalClasses: {
@@ -40,10 +34,15 @@ const props: TdStepsProps = {
     type: Boolean,
     value: false,
   },
-  /** 【讨论中】步骤条分割符 */
+  /** 步骤条分割符 */
   separator: {
     type: String,
     value: 'line',
+  },
+  /** 自定义组件样式 */
+  style: {
+    type: String,
+    value: '',
   },
   /** 步骤条风格 */
   theme: {
