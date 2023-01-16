89ff60918d4963bffcaf2aacea089671563d91cd
anlyyao <anly_yaw@163.com>
2023-01-16 21:53:14 +0800

    fix(NoticeBar): fix cr

--- src/notice-bar/_example/event/index.js
+++ src/notice-bar/_example/event/index.js
@@ -9,15 +9,7 @@ Component({
   methods: {
     click(e) {
       const { trigger } = e.detail;
-      if (trigger === 'prefix-icon') {
-        console.log('click prefix-icon text');
-      } else if (trigger === 'content') {
-        console.log('click content text');
-      } else if (trigger === 'extra') {
-        console.log('click extra text');
-      } else if (trigger === 'suffix-icon') {
-        console.log('click suffix-icon text');
-      }
+      console.log(`click on the ${trigger} area`);
     },
   },
 });
