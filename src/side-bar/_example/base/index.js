fdd137333edc3c893a0159aac9d0c91b624d91f1
zhangpaopao <zhangpaopao0609@gmail.com>
2023-02-14 10:41:50 +0800

    feat(sidebar): support aria (#1554)
    * feat(sidebar): support aria
    
    * feat(sidebar): update snap

--- src/side-bar/_example/base/index.js
+++ src/side-bar/_example/base/index.js
@@ -8,36 +8,36 @@ Page({
     scrollTop: 0,
     categories: [
       {
-        label: '选项选项选项选项',
-        title: '标题',
+        label: '选项一',
+        title: '标题一',
         badgeProps: {},
         items,
       },
       {
-        label: '选项',
-        title: '标题',
+        label: '选项二',
+        title: '标题二',
         badgeProps: {
           dot: true,
         },
         items: items.slice(0, 9),
       },
       {
-        label: '选项',
-        title: '标题',
+        label: '选项三',
+        title: '标题三',
         badgeProps: {},
         items: items.slice(0, 9),
       },
       {
-        label: '选项',
-        title: '标题',
+        label: '选项四',
+        title: '标题四',
         badgeProps: {
           count: 6,
         },
         items: items.slice(0, 6),
       },
       {
-        label: '选项',
-        title: '标题',
+        label: '选项五',
+        title: '标题五',
         badgeProps: {},
         items: items.slice(0, 3),
       },
