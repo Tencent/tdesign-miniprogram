a17871509f6726abe1968508783717b074f807cb
anlyyao <anly_yaw@163.com>
2023-02-16 11:32:07 +0800

    refactor: support class

--- src/checkbox-group/checkbox-group.ts
+++ src/checkbox-group/checkbox-group.ts
@@ -6,7 +6,7 @@ const { prefix } = config;
 const name = `${prefix}-checkbox-group`;
 @wxComponent()
 export default class CheckBoxGroup extends SuperComponent {
-  externalClasses = ['class', `${prefix}-class`];
+  externalClasses = [`${prefix}-class`];
 
   relations: RelationsOptions = {
     '../checkbox/checkbox': {
