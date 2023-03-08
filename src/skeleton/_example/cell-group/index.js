8f66e6e6345003cbbb96be28823b1aa221f4d4a4
anlyyao <anly_yaw@163.com>
2023-03-08 12:53:16 +0800

    feat(wxs): the style attr supports camel case

--- src/skeleton/_example/cell-group/index.js
+++ src/skeleton/_example/cell-group/index.js
@@ -1,7 +1,7 @@
 Component({
   data: {
-    rowColsAvater: [{ size: '48px', type: 'circle' }],
-    rowColsImage: [{ size: '48px', type: 'rect' }],
+    rowColsAvater: [{ size: '96rpx', type: 'circle' }],
+    rowColsImage: [{ size: '96rpx', type: 'rect' }],
     rowColsContent: [{ width: '50%' }, { width: '100%' }],
   },
 });
