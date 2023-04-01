010baf1694dd1ec0aa8c30082b1a2c65933e06d2
minushuang <minushuang@tencent.com>
2023-04-01 22:43:28 +0800

    feat: delete qq example

--- qq/example/pages/home/data/index.ts
+++
@@ -1,7 +0,0 @@
-import base from './base';
-import nav from './nav';
-import display from './display';
-import form from './form';
-import ux from './ux';
-
-export default [base, nav, display, form, ux];
