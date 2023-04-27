c96d70f094fd9330744642e18682fbfcc4fdd0dd
leejimqiu <leejimqiu@gmail.com>
2023-04-27 15:18:22 +0800

    fix(image-viewer): add using-custom-navation property

--- src/image-viewer/props.ts
+++ src/image-viewer/props.ts
@@ -9,15 +9,24 @@ const props: TdImageViewerProps = {
   /** 遮罩的背景颜色 */
   backgroundColor: {
     type: String,
-    optionalTypes: [Number],
     value: 'rgba(0, 0, 0, 1)',
+  },
+  /** 是否显示关闭操作，前提需要开启页码。值为字符串表示图标名称，值为 `true` 表示使用默认图标 `close`，值为 `Object` 类型，表示透传至 `icon` ，不传表示不显示图标 */
+  closeBtn: {
+    type: null,
+    value: false,
+  },
+  /** 是否显示删除操作，前提需要开启页码。值为字符串表示图标名称，值为 `true` 表示使用默认图标 `delete`，值为 `Object` 类型，表示透传至 `icon`，不传表示不显示图标 */
+  deleteBtn: {
+    type: null,
+    value: false,
   },
   /** 图片数组 */
   images: {
     type: Array,
     value: [],
   },
-  /** 默认展示第几项 */
+  /** 初始化页码 */
   initialIndex: {
     type: Number,
     value: 0,
@@ -27,14 +36,9 @@ const props: TdImageViewerProps = {
     type: Boolean,
     value: false,
   },
-  /** 是否显示删除操作 */
-  deleteBtn: {
-    type: null,
-    value: false,
-  },
-  /** 是否显示关闭操作 */
-  closeBtn: {
-    type: null,
+  /** 是否使用了自定义导航栏 */
+  usingCustomNavbar: {
+    type: Boolean,
     value: false,
   },
   /** 隐藏/显示预览 */
