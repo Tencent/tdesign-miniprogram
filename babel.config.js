cf355d1e8b1d1fb61c52a16c750677500ce3ee01
zhangpaopao <zhangpaopao0609@gmail.com>
2023-05-30 16:35:21 +0800

    fix: rename proposal to transform and display installation dependencies (#2046)

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,6 @@ module.exports = {
   presets: [['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-typescript'],
   plugins: [
     ['@babel/plugin-proposal-decorators', { legacy: true }],
-    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ['@babel/plugin-transform-class-properties', { loose: true }],
   ],
 };
