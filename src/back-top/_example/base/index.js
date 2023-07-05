edf5f15d2dbe370e34733a7e4ba6fca8cc873e16
betavs <392368711@qq.com>
2023-07-05 10:35:10 +0800

    feat(back-top): example of enable visibilityHeight and scrollTop

--- src/back-top/_example/base/index.js
+++ src/back-top/_example/base/index.js
@@ -1,4 +1,7 @@
 Component({
+  properties: {
+    scrollTop: { type: Number, value: 0 },
+  },
   data: {
     backTopTheme: 'round',
     backTopText: '顶部',
--- src/pull-down-refresh/_example/base/index.js
+++ src/pull-down-refresh/_example/base/index.js
@@ -6,12 +6,13 @@ Component({
     },
     rowCol1: [{ width: '100%', height: '342rpx', borderRadius: '24rpx' }],
     rowCol2: [[{ width: '327rpx' }], [{ width: '200rpx' }], [{ size: '327rpx', borderRadius: '24rpx' }]],
-    backTopVisible: false,
+    scrollTop: 0,
   },
 
   ready() {
+    this.setData({ enable: true });
     setTimeout(() => {
-      this.setData({ enable: true });
+      this.setData({ enable: false });
     }, 1000);
   },
 
@@ -25,9 +26,7 @@ Component({
     onScroll(e) {
       const { scrollTop } = e.detail;
 
-      this.setData({
-        backTopVisible: scrollTop > 100,
-      });
+      this.setData({ scrollTop });
     },
   },
 });
