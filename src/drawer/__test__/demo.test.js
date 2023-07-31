9f78de781516e5373da76e6850c92ea05261b550
leejimqiu <leejimqiu@gmail.com>
2023-07-31 15:20:30 +0800

    fix: update script for testing demo

--- src/drawer/__test__/demo.test.js
+++ src/drawer/__test__/demo.test.js
@@ -2,8 +2,8 @@
  * 该文件为由脚本 `npm run test:demo` 自动生成，如需修改，执行脚本命令即可。请勿手写直接修改，否则会被覆盖
  */
 
-import simulate from 'miniprogram-simulate';
 import path from 'path';
+import simulate from 'miniprogram-simulate';
 
 const mapper = ['base', 'footer', 'icon-drawer', 'title'];
 
