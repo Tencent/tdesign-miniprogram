3838794ec9194f1c03e041b5764100fca7f4f3e3
黎伟杰 <674416404@qq.com>
2024-04-09 14:03:36 +0800

    docs(result): sync api

--- src/result/type.ts
+++ src/result/type.ts
@@ -13,13 +13,6 @@ export interface TdResultProps {
     value?: string;
   };
   /**
-   * 组件类名，分别用于设置 组件外层类名、文本描述类名、图片类名、操作按钮类名。`['t-class', 't-class-image', 't-class-title', 't-class-description']`
-   */
-  externalClasses?: {
-    type: ArrayConstructor;
-    value?: array;
-  };
-  /**
    * 图标名称。值为字符串表示图标名称，值为 `false` 表示不显示图标，值为 `Object` 类型，表示透传至 `icon`，不传表示使用主题图标。
    * @default true
    */
