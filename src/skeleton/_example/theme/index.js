61674a37e60bb343e7c4b643819d0141421a5303
anlyyao <anly_yaw@163.com>
2024-04-22 12:39:43 +0800

    fix(Skeleton): fix the problem of invalid loading default value

--- src/skeleton/_example/theme/index.js
+++ src/skeleton/_example/theme/index.js
@@ -4,22 +4,18 @@ Component({
       {
         title: '头像骨架屏',
         value: 'avatar',
-        loading: true,
       },
       {
         title: '图片骨架屏',
         value: 'image',
-        loading: true,
       },
       {
         title: '文本骨架屏',
         value: 'text',
-        loading: true,
       },
       {
         title: '段落骨架屏',
         value: 'paragraph',
-        loading: true,
       },
     ],
   },
