62894e94a0e002cb50892a4a317d318014e2d25c
betavs <34408516+betavs@users.noreply.github.com>
2024-05-28 15:57:34 +0800

    fix(icon): dynamically set image size (#2806)
    * fix(icon): dynamically set image size
    
    * test(icon): dynamically set image size

--- src/icon/icon.ts
+++ src/icon/icon.ts
@@ -1,7 +1,7 @@
 import { SuperComponent, wxComponent } from '../common/src/index';
 import config from '../common/config';
 import props from './props';
-import { styles, addUnit } from '../common/utils';
+import { styles, addUnit, getRect } from '../common/utils';
 
 const { prefix } = config;
 const name = `${prefix}-icon`;
@@ -30,20 +30,26 @@ export default class Icon extends SuperComponent {
       this.triggerEvent('click', event.detail);
     },
 
-    setIconStyle() {
-      const { name, color, size } = this.properties;
+    async setIconStyle() {
+      const { name, color, size, classPrefix } = this.data;
       const isImage = name.indexOf('/') !== -1;
+
       const sizeValue = addUnit(size);
-      const sizeStyle = isImage && sizeValue ? { width: sizeValue, height: sizeValue } : {};
       const colorStyle = color ? { color: color } : {};
       const fontStyle = size ? { 'font-size': sizeValue } : {};
+      const iconStyle: Record<string, any> = { ...colorStyle, ...fontStyle };
+
+      if (isImage) {
+        const { height } = await getRect(this, `.${classPrefix}`);
+        const iconSize = sizeValue || addUnit(height);
+
+        iconStyle.width = iconSize;
+        iconStyle.height = iconSize;
+      }
+
       this.setData({
         isImage,
-        iconStyle: `${styles({
-          ...colorStyle,
-          ...fontStyle,
-          ...sizeStyle,
-        })}`,
+        iconStyle: `${styles(iconStyle)}`,
       });
     },
   };
