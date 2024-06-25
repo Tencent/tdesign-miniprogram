eea9db0d1ac50a3999543bab69c6da0e745ee21f
Lyan-u <liyan_mr@163.com>
2024-06-25 11:01:52 +0800

    feat(swipe-cell): support dragstart and dragend events

--- src/swipe-cell/props.ts
+++ src/swipe-cell/props.ts
@@ -16,13 +16,17 @@ const props: TdSwipeCellProps = {
   },
   /** 操作项是否呈现为打开态，值为数组时表示分别控制左右滑动的展开和收起状态 */
   opened: {
-    type: Boolean,
-    optionalTypes: [Array],
+    type: null,
     value: false,
   },
-  /** 右侧滑动操作项。有两种定义方式，一种是使用数组，二种是使用插槽。`right.text` 表示操作文本，`right.className` 表示操作项类名，`right.style` 表示操作项样式，`right.onClick` 表示点击操作项后执行的回调函数。示例：`[{ text: '删除', style: 'background-color: red', onClick: () => {} }]` */
+  /** 右侧滑动操作项。有两种定义方式，一种是使用数组，二种是使用插槽。`right.text` 表示操作文本，`right.className` 表示操作项类名，`right.style` 表示操作项样式，`right.onClick` 表示点击操作项后执行的回调函数。示例：`[{ text: '删除', icon: 'delete', style: 'background-color: red', onClick: () => {} }]` */
   right: {
     type: Array,
+  },
+  /** 自定义组件样式 */
+  style: {
+    type: String,
+    value: '',
   },
 };
 
