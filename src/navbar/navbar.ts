c2cbc5237cdb24b26e24305919b24e48f7bbd40d
jby0107 <50822648+jby0107@users.noreply.github.com>
2024-07-01 14:24:21 +0800

    feat(navbar): adapt miniprogram's capsule length change feature (#2922)
    * feat(navbar): adapt miniprogram's capsule length change feature
    
    wechat add a new feature of displaying the provider of the miniprogram on the capsule. When it
    displays, the capsule enxtends above the content of the navbar, this commit adapts this new feature
    , hides the content of the navbar when it is covered by the capsule
    
    "feat #2802"
    
    * feat(navbar): dapt miniprogram's capsule length change feature
    
    wechat add a new feature of displaying the provider of the miniprogram on the capsule. When it
    displays, the capsule enxtends above the content of the navbar, this commit adapts this new feature
    , hides the content of the navbar when it is covered by the capsule
    
    "feat #2802"
    
    * feat(navbar): update snap
    
    * feat(navbar): use getRect adaptor
    
    ---------
    
    Co-authored-by: jarmywang <awangjianjun9@gmail.com>

--- src/navbar/navbar.ts
+++ src/navbar/navbar.ts
@@ -1,4 +1,5 @@
 import { SuperComponent, wxComponent } from '../common/src/index';
+import { getRect } from '../common/utils';
 import config from '../common/config';
 import props from './props';
 
@@ -62,6 +63,8 @@ export default class Navbar extends SuperComponent {
     classPrefix: name,
     boxStyle: '',
     showTitle: '',
+    hideLeft: false, // 隐藏左侧内容
+    hideCenter: false, // 隐藏中部内容
   };
 
   attached() {
@@ -86,6 +89,12 @@ export default class Navbar extends SuperComponent {
         this.setData({
           boxStyle: `${boxStyleList.join('; ')}`,
         });
+        // @ts-ignore
+        if (wx.onMenuButtonBoundingClientRectWeightChange) {
+          // fixme: 规避单元测试无法识别新api，更新后可删除
+          // @ts-ignore
+          wx.onMenuButtonBoundingClientRectWeightChange((res: object) => this.queryElements(res)); // 监听胶囊条长度变化，隐藏遮挡的内容
+        }
       },
       fail: (err) => {
         console.error('navbar 获取系统信息失败', err);
@@ -93,7 +102,43 @@ export default class Navbar extends SuperComponent {
     });
   }
 
+  detached() {
+    // @ts-ignore
+    if (wx.offMenuButtonBoundingClientRectWeightChange) {
+      // fixme: 规避单元测试无法识别新api，更新后可删除
+      // @ts-ignore
+      wx.offMenuButtonBoundingClientRectWeightChange((res: object) => this.queryElements(res));
+    }
+  }
+
   methods = {
+    /**
+     * 比较胶囊条和navbar内容，决定是否隐藏
+     * @param capsuleRect API返回值，胶囊条的位置信息
+     */
+    queryElements(capsuleRect) {
+      Promise.all([
+        getRect(this, `.${this.data.classPrefix}__left`),
+        getRect(this, `.${this.data.classPrefix}__center`),
+      ]).then(([leftRect, centerRect]) => {
+        if (leftRect.right > capsuleRect.left) {
+          this.setData({
+            hideLeft: true,
+            hideCenter: true,
+          });
+        } else if (centerRect.right > capsuleRect.left) {
+          this.setData({
+            hideLeft: false,
+            hideCenter: true,
+          });
+        } else {
+          this.setData({
+            hideLeft: false,
+            hideCenter: false,
+          });
+        }
+      });
+    },
     goBack() {
       const { delta } = this.data;
       // eslint-disable-next-line
