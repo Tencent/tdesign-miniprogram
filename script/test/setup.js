1a200f62e7365fd170ed6e8e9f756b78739492c5
anlyyao <anly_yaw@163.com>
2024-07-05 15:11:58 +0800

    test: mock getMenuButtonBoundingClientRect

--- script/test/setup.js
+++ script/test/setup.js
@@ -16,6 +16,7 @@ global.load = (path, demoName) => {
   });
 };
 
+// 测试环境配置：iPhone 6/7/8 机型
 const systemInfo = {
   SDKVersion: '2.19.1',
   batteryLevel: 100,
@@ -23,21 +24,33 @@ const systemInfo = {
   brand: 'devtools',
   fontSizeSetting: 16,
   language: 'zh_CN',
-  model: 'iPhone 7 Plus',
-  pixelRatio: 3,
+  model: 'iPhone 6/7/8',
+  pixelRatio: 2,
   platform: 'devtools',
-  screenHeight: 736,
-  screenWidth: 414,
+  screenHeight: 667,
+  screenTop: 0,
+  screenWidth: 375,
   statusBarHeight: 20,
   system: 'iOS 10.0.1',
-  version: '6.6.3',
-  windowHeight: 672,
-  windowWidth: 414,
+  theme: 'light',
+  version: '8.0.5',
+  windowHeight: 667,
+  windowWidth: 375,
+};
+
+const menuButtonBoundingClientRect = {
+  bottom: 56,
+  height: 32,
+  left: 281,
+  right: 368,
+  top: 24,
+  width: 87,
 };
 
 global.wx = {
   ...similateApi,
-  getSystemInfoSync: mockSync(systemInfo),
   getSystemInfo: mockAsyncAndPromise('getSystemInfo', systemInfo),
+  getSystemInfoSync: mockSync(systemInfo),
+  getMenuButtonBoundingClientRect: mockSync(menuButtonBoundingClientRect),
 };
 global.VIRTUAL_HOST = canUseVirtualHost();
