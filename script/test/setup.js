6d7bcc03dbfe0525bdbfc46dcfebbeb751c7f71c
anlyyao <anly_yaw@163.com>
2024-07-05 14:53:41 +0800

    chore(Navbar): simplify the code

--- script/test/setup.js
+++ script/test/setup.js
@@ -1,6 +1,7 @@
 import Path from 'path';
 import simulate from 'miniprogram-simulate';
 import similateApi from 'miniprogram-simulate/src/api';
+import { mockSync, mockAsyncAndPromise } from 'miniprogram-simulate/src/api/utils';
 import { canUseVirtualHost } from '../../src/common/version';
 
 global.getApp = () => null;
@@ -14,27 +15,29 @@ global.load = (path, demoName) => {
     },
   });
 };
+
+const systemInfo = {
+  SDKVersion: '2.19.1',
+  batteryLevel: 100,
+  benchmarkLevel: 1,
+  brand: 'devtools',
+  fontSizeSetting: 16,
+  language: 'zh_CN',
+  model: 'iPhone 7 Plus',
+  pixelRatio: 3,
+  platform: 'devtools',
+  screenHeight: 736,
+  screenWidth: 414,
+  statusBarHeight: 20,
+  system: 'iOS 10.0.1',
+  version: '6.6.3',
+  windowHeight: 672,
+  windowWidth: 414,
+};
+
 global.wx = {
   ...similateApi,
-  getSystemInfoSync: () => {
-    return {
-      SDKVersion: '2.19.1',
-      batteryLevel: 100,
-      benchmarkLevel: 1,
-      brand: 'devtools',
-      fontSizeSetting: 16,
-      language: 'zh_CN',
-      model: 'iPhone 7 Plus',
-      pixelRatio: 3,
-      platform: 'devtools',
-      screenHeight: 736,
-      screenWidth: 414,
-      statusBarHeight: 20,
-      system: 'iOS 10.0.1',
-      version: '6.6.3',
-      windowHeight: 672,
-      windowWidth: 414,
-    };
-  },
+  getSystemInfoSync: mockSync(systemInfo),
+  getSystemInfo: mockAsyncAndPromise('getSystemInfo', systemInfo),
 };
 global.VIRTUAL_HOST = canUseVirtualHost();
