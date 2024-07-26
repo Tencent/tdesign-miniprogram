12451d4815919748ce13eb7f8f00e48bd8a731e1
jarmywang <awangjianjun9@gmail.com>
2024-07-26 14:41:03 +0800

    fix(Indexes): resolved the problem of position confusion and jamming (#3000)
    * fix(Indexes): resolved the problem of position confusion and jamming
    
    * fix(Indexes): resolved the problem of position confusion and jamming
    
    * fix(Indexes): resolved the problem of position confusion and jamming
    
    * fix: fix style for demo
    
    ---------
    
    Co-authored-by: anlyyao <anly_yaw@163.com>

--- src/indexes/indexes.ts
+++ src/indexes/indexes.ts
@@ -37,6 +37,8 @@ export default class Indexes extends SuperComponent {
 
   sidebar = null;
 
+  currentTouchAnchor = null;
+
   observers = {
     indexList(v) {
       this.setIndexList(v);
@@ -49,6 +51,9 @@ export default class Indexes extends SuperComponent {
 
   lifetimes = {
     ready() {
+      this.timer = null;
+      this.groupTop = [];
+      this.sidebar = null;
       if (this.data._height === 0) {
         this.setHeight();
       }
@@ -143,25 +148,23 @@ export default class Indexes extends SuperComponent {
     },
 
     setAnchorByIndex(index) {
-      if (this.preIndex != null && this.preIndex === index) return;
-
-      const { _indexList } = this.data;
+      const { _indexList, stickyOffset } = this.data;
       const activeAnchor = _indexList[index];
+
+      if (this.data.activeAnchor !== null && this.data.activeAnchor === activeAnchor) return;
+
       const target = this.groupTop.find((item) => item.anchor === activeAnchor);
 
       if (target) {
+        this.currentTouchAnchor = activeAnchor;
+        const scrollTop = target.top - stickyOffset;
         wx.pageScrollTo({
-          scrollTop: target.top,
+          scrollTop,
           duration: 0,
         });
-      }
-
-      this.preIndex = index;
-      this.toggleTips(true);
-      this.triggerEvent('select', { index: activeAnchor });
-
-      if (activeAnchor !== this.data.activeAnchor) {
-        this.triggerEvent('change', { index: activeAnchor });
+        this.toggleTips(true);
+        this.triggerEvent('select', { index: activeAnchor });
+        this.setData({ activeAnchor });
       }
     },
 
@@ -208,7 +211,7 @@ export default class Indexes extends SuperComponent {
         return;
       }
 
-      const { sticky, stickyOffset } = this.data;
+      const { sticky, stickyOffset, activeAnchor } = this.data;
 
       scrollTop += stickyOffset;
 
@@ -219,14 +222,13 @@ export default class Indexes extends SuperComponent {
       if (curIndex === -1) return;
 
       const curGroup = this.groupTop[curIndex];
-
-      if (this.data.activeAnchor !== curGroup.anchor) {
+      if (this.currentTouchAnchor !== null) {
         this.triggerEvent('change', { index: curGroup.anchor });
+        this.currentTouchAnchor = null;
+      } else if (activeAnchor !== curGroup.anchor) {
+        this.triggerEvent('change', { index: curGroup.anchor });
+        this.setData({ activeAnchor: curGroup.anchor });
       }
-
-      this.setData({
-        activeAnchor: curGroup.anchor,
-      });
 
       if (sticky) {
         const offset = curGroup.top - scrollTop;
@@ -234,22 +236,29 @@ export default class Indexes extends SuperComponent {
 
         this.$children.forEach((child, index) => {
           if (index === curIndex) {
-            child.setData({
-              sticky: scrollTop > stickyOffset,
-              active: true,
-              style: `height: ${curGroup.height}px`,
-              anchorStyle: `transform: translate3d(0, ${betwixt ? offset : 0}px, 0); top: ${stickyOffset}px`,
-            });
+            const sticky = scrollTop > stickyOffset;
+            const anchorStyle = `transform: translate3d(0, ${betwixt ? offset : 0}px, 0); top: ${stickyOffset}px`;
+            if (anchorStyle !== child.data.anchorStyle || sticky !== child.data.sticky) {
+              child.setData({
+                sticky,
+                active: true,
+                style: `height: ${curGroup.height}px`,
+                anchorStyle,
+              });
+            }
           } else if (index + 1 === curIndex) {
             // 两个 anchor 同时出现时的上一个
-            child.setData({
-              sticky: true,
-              active: true,
-              style: `height: ${curGroup.height}px`,
-              anchorStyle: `transform: translate3d(0, ${
-                betwixt ? offset - curGroup.height : 0
-              }px, 0); top: ${stickyOffset}px`,
-            });
+            const anchorStyle = `transform: translate3d(0, ${
+              betwixt ? offset - curGroup.height : 0
+            }px, 0); top: ${stickyOffset}px`;
+            if (anchorStyle !== child.data.anchorStyle) {
+              child.setData({
+                sticky: true,
+                active: true,
+                style: `height: ${curGroup.height}px`,
+                anchorStyle,
+              });
+            }
           } else {
             child.setData({ active: false, sticky: false, anchorStyle: '' });
           }
