471b79956398187202edc7d77dedbe78becdd4ec
novlan1 <1576271227@qq.com>
2024-08-08 17:51:09 +0800

    feat(message): 支持组件调用

--- src/message/message.ts
+++ src/message/message.ts
@@ -9,6 +9,7 @@ const SHOW_DURATION = 400;
 const { prefix } = config;
 const name = `${prefix}-message`;
 
+let idIndex = 0;
 @wxComponent()
 export default class Message extends SuperComponent {
   options: ComponentsOptionsType = {
@@ -32,7 +33,50 @@ export default class Message extends SuperComponent {
   // 两条message之间的间距，单位px
   gap = 12;
 
-  observers = {};
+  observers = {
+    visible(value) {
+      const id = `t-message-component-${idIndex}`;
+
+      if (value) {
+        this.setData(
+          {
+            messageList: [
+              {
+                id,
+              },
+            ],
+          },
+          () => {
+            const instance = this.selectComponent(`#${id}`);
+            const { content, align, theme, icon, closeBtn, action, marquee, offset, duration, zIndex, gap, single } =
+              this.properties;
+
+            instance.setData(
+              {
+                content,
+                align,
+                theme,
+                icon,
+                closeBtn,
+                action,
+                marquee,
+                offset,
+                duration,
+                zIndex,
+                gap,
+                single,
+              },
+              instance.show.bind(instance, 0),
+            );
+          },
+        );
+      } else {
+        this.setData({
+          messageList: [],
+        });
+      }
+    },
+  };
 
   pageLifetimes = {
     show() {
@@ -52,6 +96,7 @@ export default class Message extends SuperComponent {
       ...this.properties,
       ...this.data,
     };
+    idIndex += 1;
   }
 
   /**
