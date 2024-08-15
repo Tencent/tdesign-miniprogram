eb33702b62414c02ae0434789c5737948c85a92b
WeybiJiang <jiangby03@sina.com>
2024-08-15 14:56:51 +0800

    fix(badge): fixed the overflow problem of ribbon badge
    fix #3063

--- src/badge/badge.ts
+++ src/badge/badge.ts
@@ -2,7 +2,7 @@ import { SuperComponent, wxComponent } from '../common/src/index';
 import config from '../common/config';
 import props from './props';
 import type { TdBadgeProps } from './type';
-import { uniqueFactory } from '../common/utils';
+import { getRect, uniqueFactory } from '../common/utils';
 
 const { prefix } = config;
 const name = `${prefix}-badge`;
@@ -26,11 +26,24 @@ export default class Badge extends SuperComponent {
     value: '',
     labelID: '',
     descriptionID: '',
+    // ribbon
+    ribbonStyle: '',
+    ribbonOuterStyle: '',
   };
 
   lifetimes = {
     ready() {
       const uniqueID = getUniqueID();
+      if (this.properties.shape === 'ribbon') {
+        getRect(this, `.${prefix}-badge--ribbon`).then((rect) => {
+          const outerBoundingRect = rect.width / Math.SQRT2 + rect.height * Math.SQRT2; // 外接矩形的宽度：height * cos45deg + width / cos45deg
+          const translateX = rect.width - rect.width / Math.SQRT2 + outerBoundingRect - rect.width; // 旋转后的位移：原宽度 - 旋转后的宽度 + 外接矩形的宽度 - 原宽度
+          this.setData({
+            ribbonStyle: `transform: translateX(${translateX}px) rotate(45deg);`,
+            ribbonOuterStyle: `width: ${outerBoundingRect}px; height: ${outerBoundingRect}px;`,
+          });
+        });
+      }
 
       this.setData({
         labelID: `${uniqueID}_label`,
