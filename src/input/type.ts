5d7ad81b639dd994248d0c7fd5fd483560044c39
anlyyao <anly_yaw@163.com>
2024-09-03 11:17:07 +0800

    docs(Input): sync api docs

--- src/input/type.ts
+++ src/input/type.ts
@@ -6,6 +6,14 @@
 
 export interface TdInputProps {
   /**
+   * 键盘弹起时，是否自动上推页面
+   * @default true
+   */
+  adjustPosition?: {
+    type: BooleanConstructor;
+    value?: boolean;
+  };
+  /**
    * 文本内容位置，居左/居中/居右
    * @default left
    */
@@ -14,31 +22,31 @@ export interface TdInputProps {
     value?: 'left' | 'center' | 'right';
   };
   /**
-   * 标题输入框布局方式
-   * @default horizontal
+   * 强制 input 处于同层状态，默认 focus 时 input 会切到非同层状态 (仅在 iOS 下生效)
+   * @default false
    */
-  layout?: {
-    type: StringConstructor;
-    value?: 'horizontal' | 'vertical';
+  alwaysEmbed?: {
+    type: BooleanConstructor;
+    value?: boolean;
+  };
+  /**
+   * (即将废弃，请直接使用 focus )自动聚焦，拉起键盘
+   * @default false
+   */
+  autoFocus?: {
+    type: BooleanConstructor;
+    value?: boolean;
   };
   /**
    * 是否开启无边框模式
-   * @default true
+   * @default false
    */
   borderless?: {
     type: BooleanConstructor;
     value?: boolean;
   };
   /**
-   * 是否可清空
-   * @default false
-   */
-  clearable?: {
-    type: null;
-    value?: boolean | object;
-  };
-  /**
-   * 清空图标触发方式
+   * 清空图标触发方式，仅在输入框有值时有效
    * @default always
    */
   clearTrigger?: {
@@ -46,12 +54,44 @@ export interface TdInputProps {
     value?: 'always' | 'focus';
   };
   /**
-   * 自定义组件样式
-   * @default ''
+   * 是否可清空，默认不启动。值为 `true` 表示使用默认清除空按钮，值为 `Object` 表示透传至 `icon`
+   * @default false
    */
-  style?: {
+  clearable?: {
+    type: null;
+    value?: boolean | object;
+  };
+  /**
+   * 点击键盘右下角按钮时是否保持键盘不收起
+   * @default false
+   */
+  confirmHold?: {
+    type: BooleanConstructor;
+    value?: boolean;
+  };
+  /**
+   * 设置键盘右下角按钮的文字，仅在type='text'时生效。<br />具体释义：<br />`send` 右下角按钮为“发送”；<br />`search` 右下角按钮为“搜索”；<br />`next` 右下角按钮为“下一个”；<br />`go` 右下角按钮为“前往”；<br />`done` 右下角按钮为“完成”。<br />[小程序官方文档](https://developers.weixin.qq.com/miniprogram/dev/component/input.html)
+   * @default done
+   */
+  confirmType?: {
     type: StringConstructor;
-    value?: string;
+    value?: 'send' | 'search' | 'next' | 'go' | 'done';
+  };
+  /**
+   * 指定 focus 时的光标位置
+   */
+  cursor: {
+    type: NumberConstructor;
+    value?: number;
+    required?: boolean;
+  };
+  /**
+   * 指定光标与键盘的距离，取 input 距离底部的距离和 cursor-spacing 指定的距离的最小值作为光标与键盘的距离
+   * @default 0
+   */
+  cursorSpacing?: {
+    type: NumberConstructor;
+    value?: number;
   };
   /**
    * 是否禁用输入框
@@ -62,27 +102,27 @@ export interface TdInputProps {
     value?: boolean;
   };
   /**
-   * 错误提示文本，值为空不显示（废弃属性，如果需要，请更为使用 status 和 tips）
-   * @default ''
-   * @deprecated
+   * 获取焦点
+   * @default false
    */
-  errorMessage?: {
-    type: StringConstructor;
-    value?: string;
-  };
-  /**
-   * 组件类名，用于设置组件外层元素、输入框、占位符、错误信息等元素类名
-   */
-  externalClasses?: {
-    type: ArrayConstructor;
-    value?: ['t-class', 't-class-input', 't-class-placeholder', 't-class-error-msg'];
+  focus?: {
+    type: BooleanConstructor;
+    value?: boolean;
   };
   /**
    * 【开发中】指定输入框展示值的格式
    */
   format?: {
-    type: null;
+    type: undefined;
     value?: InputFormatType;
+  };
+  /**
+   * focus时，点击页面的时候不收起键盘
+   * @default false
+   */
+  holdKeyboard?: {
+    type: BooleanConstructor;
+    value?: boolean;
   };
   /**
    * 左侧文本
@@ -92,6 +132,14 @@ export interface TdInputProps {
     value?: string;
   };
   /**
+   * 标题输入框布局方式
+   * @default horizontal
+   */
+  layout?: {
+    type: StringConstructor;
+    value?: 'vertical' | 'horizontal';
+  };
+  /**
    * 用户最多可以输入的字符个数，一个中文汉字表示两个字符长度。`maxcharacter` 和 `maxlength` 二选一使用
    */
   maxcharacter?: {
@@ -99,7 +147,7 @@ export interface TdInputProps {
     value?: number;
   };
   /**
-   * 用户最多可以输入的文本长度，一个中文等于一个计数长度，默认为 -1，不限制输入长度。`maxcharacter` 和 `maxlength` 二选一使用
+   * 用户最多可以输入的文本长度，一个中文等于一个计数长度。默认为 -1，不限制输入长度。`maxcharacter` 和 `maxlength` 二选一使用
    * @default -1
    */
   maxlength?: {
@@ -114,14 +162,31 @@ export interface TdInputProps {
     value?: string;
   };
   /**
-   * 组件前置图标，值为字符串则表示图标名称
+   * 指定 placeholder 的样式类
+   * @default input-placeholder
+   */
+  placeholderClass?: {
+    type: StringConstructor;
+    value?: string;
+  };
+  /**
+   * 指定 placeholder 的样式
+   * @default ''
+   */
+  placeholderStyle: {
+    type: StringConstructor;
+    value?: string;
+    required?: boolean;
+  };
+  /**
+   * 组件前置图标。值为字符串表示图标名称，值为 `Object` 类型，表示透传至 `icon`
    */
   prefixIcon?: {
     type: null;
     value?: string | object;
   };
   /**
-   * 只读状态
+   * 【开发中】只读状态
    * @default false
    */
   readonly?: {
@@ -129,20 +194,73 @@ export interface TdInputProps {
     value?: boolean;
   };
   /**
-   * 输入框尺寸
-   * @default small
+   * 安全键盘加密公钥的路径，只支持包内路径
+   * @default ''
    */
-  size?: {
+  safePasswordCertPath?: {
     type: StringConstructor;
-    value?: 'medium' | 'small';
+    value?: string;
+  };
+  /**
+   * 安全键盘计算 hash 的算法表达式，如 `md5(sha1('foo' + sha256(sm3(password + 'bar'))))`
+   * @default ''
+   */
+  safePasswordCustomHash?: {
+    type: StringConstructor;
+    value?: string;
+  };
+  /**
+   * 安全键盘输入密码长度
+   */
+  safePasswordLength?: {
+    type: NumberConstructor;
+    value?: number;
+  };
+  /**
+   * 安全键盘加密盐值
+   * @default ''
+   */
+  safePasswordNonce?: {
+    type: StringConstructor;
+    value?: string;
+  };
+  /**
+   * 安全键盘计算 hash 盐值，若指定custom-hash 则无效
+   * @default ''
+   */
+  safePasswordSalt?: {
+    type: StringConstructor;
+    value?: string;
+  };
+  /**
+   * 安全键盘加密时间戳
+   */
+  safePasswordTimeStamp?: {
+    type: NumberConstructor;
+    value?: number;
+  };
+  /**
+   * 光标结束位置，自动聚集时有效，需与 selection-start 搭配使用
+   * @default -1
+   */
+  selectionEnd?: {
+    type: NumberConstructor;
+    value?: number;
+  };
+  /**
+   * 光标起始位置，自动聚集时有效，需与 selection-end 搭配使用
+   * @default -1
+   */
+  selectionStart?: {
+    type: NumberConstructor;
+    value?: number;
   };
   /**
    * 输入框状态
-   * @default default
    */
   status?: {
     type: StringConstructor;
-    value?: 'default' | 'success' | 'warning' | 'error';
+    value?: 'success' | 'warning' | 'error';
   };
   /**
    * 后置图标前的后置内容
@@ -152,7 +270,7 @@ export interface TdInputProps {
     value?: string;
   };
   /**
-   * 后置文本内容，值为字符串则表示图标名称
+   * 后置文本内容。值为字符串则表示图标名称，值为 `Object` 类型，表示透传至 `icon`。
    */
   suffixIcon?: {
     type: null;
@@ -177,169 +295,11 @@ export interface TdInputProps {
    * 输入框的值
    */
   value?: {
-    type: StringConstructor;
-    optionalTypes: Array<NumberConstructor>;
+    type: null;
     value?: InputValue;
-  };
-  /**
-   * 输入框的值，非受控属性
-   */
-  defaultValue?: {
-    type: StringConstructor;
-    optionalTypes: Array<NumberConstructor>;
-    value?: InputValue;
-  };
-  /**
-   * 指定 placeholder 的样式
-   * @default ''
-   */
-  placeholderStyle: {
-    type: StringConstructor;
-    value?: string;
-  };
-  /**
-   * 指定 placeholder 的样式类
-   * @default input-placeholder
-   */
-  placeholderClass?: {
-    type: StringConstructor;
-    value?: string;
-  };
-  /**
-   * 指定光标与键盘的距离，取 input 距离底部的距离和 cursor-spacing 指定的距离的最小值作为光标与键盘的距离
-   * @default 0
-   */
-  cursorSpacing?: {
-    type: NumberConstructor;
-    value?: number;
-  };
-  /**
-   * (即将废弃，请直接使用 focus )自动聚焦，拉起键盘
-   * @default false
-   */
-  autoFocus?: {
-    type: BooleanConstructor;
-    value?: boolean;
-  };
-  /**
-   * 获取焦点
-   * @default false
-   */
-  focus?: {
-    type: BooleanConstructor;
-    value?: boolean;
-  };
-  /**
-   * 设置键盘右下角按钮的文字，仅在type='text'时生效。<br />具体释义：<br />`send` 右下角按钮为“发送”；<br />`search` 右下角按钮为“搜索”；<br />`next` 右下角按钮为“下一个”；<br />`go` 右下角按钮为“前往”；<br />`done` 右下角按钮为“完成”。<br />[小程序官方文档](https://developers.weixin.qq.com/miniprogram/dev/component/input.html)
-   * @default done
-   */
-  confirmType?: {
-    type: StringConstructor;
-    value?: 'send' | 'search' | 'next' | 'go' | 'done';
-  };
-  /**
-   * 强制 input 处于同层状态，默认 focus 时 input 会切到非同层状态 (仅在 iOS 下生效)
-   * @default false
-   */
-  alwaysEmbed?: {
-    type: BooleanConstructor;
-    value?: boolean;
-  };
-  /**
-   * 点击键盘右下角按钮时是否保持键盘不收起
-   * @default false
-   */
-  confirmHold?: {
-    type: BooleanConstructor;
-    value?: boolean;
-  };
-  /**
-   * 指定focus时的光标位置
-   */
-  cursor: {
-    type: NumberConstructor;
-    value?: number;
-  };
-  /**
-   * 光标起始位置，自动聚集时有效，需与selection-end搭配使用
-   * @default -1
-   */
-  selectionStart?: {
-    type: NumberConstructor;
-    value?: number;
-  };
-  /**
-   * 光标结束位置，自动聚集时有效，需与selection-start搭配使用
-   * @default -1
-   */
-  selectionEnd?: {
-    type: NumberConstructor;
-    value?: number;
-  };
-  /**
-   * 键盘弹起时，是否自动上推页面
-   * @default true
-   */
-  adjustPosition?: {
-    type: BooleanConstructor;
-    value?: boolean;
-  };
-  /**
-   * focus时，点击页面的时候不收起键盘
-   * @default false
-   */
-  holdKeyboard?: {
-    type: BooleanConstructor;
-    value?: boolean;
-  };
-  /**
-   * 安全键盘加密公钥的路径，只支持包内路径
-   * @default ''
-   */
-  safePasswordCertPath?: {
-    type: StringConstructor;
-    value?: string;
-  };
-  /**
-   * 安全键盘输入密码长度
-   */
-  safePasswordLength?: {
-    type: NumberConstructor;
-    value?: number;
-  };
-  /**
-   * 安全键盘加密时间戳
-   */
-  safePasswordTimeStamp?: {
-    type: NumberConstructor;
-    value?: number;
-  };
-  /**
-   * 安全键盘加密盐值
-   * @default ''
-   */
-  safePasswordNonce?: {
-    type: StringConstructor;
-    value?: string;
-  };
-  /**
-   * 安全键盘计算hash盐值，若指定custom-hash 则无效
-   * @default ''
-   */
-  safePasswordSalt?: {
-    type: StringConstructor;
-    value?: string;
-  };
-  /**
-   * 安全键盘计算hash的算法表达式，如 `md5(sha1('foo' + sha256(sm3(password + 'bar'))))`
-   * @default ''
-   */
-  safePasswordCustomHash?: {
-    type: StringConstructor;
-    value?: string;
   };
 }
 
-export type InputFormatType = (value: InputValue) => number | string;
+export type InputFormatType = (value: InputValue) => string;
 
 export type InputValue = string | number;
