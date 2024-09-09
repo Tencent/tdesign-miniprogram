abe1f5369fa453f5cd0b45046cb79dd1b23d581f
Y. <anly_yaw@163.com>
2024-09-09 14:29:58 +0800

    feat(PickerItem): support slot (#3127)
    * feat(PickerItem): support slot
    
    * test: update snapshots

--- src/picker/_example/base/index.js
+++ src/picker/_example/base/index.js
@@ -5,8 +5,8 @@ Component({
     dateText: '',
     dateValue: [],
     citys: [
-      { label: '北京市', value: '北京市' },
-      { label: '上海市', value: '上海市' },
+      { label: '北京市', value: '北京市', tag: '合' },
+      { label: '上海市', value: '上海市', tag: '合' },
       { label: '广州市', value: '广州市' },
       { label: '深圳市', value: '深圳市' },
       { label: '成都市', value: '成都市' },
