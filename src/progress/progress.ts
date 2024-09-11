d8738f7552a6a873fc8f84f312e880e74dfae887
huxinhai <86554234+huxinhai@users.noreply.github.com>
2024-09-11 14:47:59 +0800

    fix(progress): 移除getInnerDiameter() 并改使用heightBar (#3139)
    * fix(progress): 移除getInnerDiameter() 并改使用heightBar
    
    * fix(progress): 移除getRect
    
    ---------
    
    Co-authored-by: hxh <1047739781>

--- src/progress/progress.ts
+++ src/progress/progress.ts
@@ -2,7 +2,7 @@ import { SuperComponent, wxComponent } from '../common/src/index';
 import config from '../common/config';
 import props from './props';
 import { getBackgroundColor } from './utils';
-import { unitConvert, getRect } from '../common/utils';
+import { unitConvert } from '../common/utils';
 
 const { prefix } = config;
 const name = `${prefix}-progress`;
@@ -67,11 +67,6 @@ export default class Progress extends SuperComponent {
       });
     },
 
-    theme(theme) {
-      if (theme === 'circle') {
-        this.getInnerDiameter();
-      }
-    },
 
     trackColor(trackColor) {
       this.setData({
@@ -80,17 +75,4 @@ export default class Progress extends SuperComponent {
     },
   };
 
-  methods = {
-    getInnerDiameter() {
-      const { strokeWidth } = this.properties;
-      const wrapID = `.${name}__canvas--circle`;
-      if (strokeWidth) {
-        getRect(this, wrapID).then((wrapRect) => {
-          this.setData({
-            innerDiameter: wrapRect.width - unitConvert(strokeWidth) * 2,
-          });
-        });
-      }
-    },
-  };
 }
