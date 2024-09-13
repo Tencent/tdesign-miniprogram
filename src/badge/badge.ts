62d76d99ad948b5c14e00966ca9b11ddee5b0d1f
anlyyao <anly_yaw@163.com>
2024-09-13 19:38:44 +0800

    chore: remove unused code

--- src/badge/badge.ts
+++ src/badge/badge.ts
@@ -26,31 +26,11 @@ export default class Badge extends SuperComponent {
     value: '',
     labelID: '',
     descriptionID: '',
-    // ribbon
-    // ribbonStyle: '',
-    // ribbonOuterStyle: '',
   };
 
   lifetimes = {
     ready() {
       const uniqueID = getUniqueID();
-      // skyline render 暂不支持 clip-path，暂时改用伪元素实现，issue #3063 场景，需手动处理溢出隐藏
-      // if (this.properties.shape === 'ribbon') {
-      //   getRect(this, `.${prefix}-badge--ribbon`).then((rect) => {
-      //     const outerBoundingRect = rect.width / Math.SQRT2 + rect.height * Math.SQRT2; // 外接矩形的宽度：height * cos45deg + width / cos45deg
-      //     const translateX = rect.width - rect.width / Math.SQRT2 + outerBoundingRect - rect.width; // 旋转后的位移：原宽度 - 旋转后的宽度 + 外接矩形的宽度 - 原宽度
-      //     const ribbonHeightHypotenuse = rect.height * Math.SQRT2; // 斜边的长度：height * sin45deg
-      //     const ribbonWidthCatheti = rect.width / Math.SQRT2; // 直角边的长度：width / sin45deg
-      //     this.setData({
-      //       ribbonStyle: `transform: translateX(${translateX}px) rotate(45deg);`,
-      //       ribbonOuterStyle: `width: ${outerBoundingRect}px; height: ${outerBoundingRect}px;
-      //         clip-path: polygon(0 0, ${ribbonHeightHypotenuse}px 0,
-      //         ${outerBoundingRect}px ${ribbonWidthCatheti}px,
-      //         ${outerBoundingRect}px ${ribbonWidthCatheti + ribbonHeightHypotenuse}px);`,
-      //     });
-      //   });
-      // }
-
       this.setData({
         labelID: `${uniqueID}_label`,
         descriptionID: `${uniqueID}_description`,
