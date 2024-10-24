f11d246bcb15681c596a0c870026bd66cd958c33
anlyyao <anly_yaw@163.com>
2024-10-24 21:09:36 +0800

    feat(DateTimePicker): additional examples

--- src/date-time-picker/_example/year-month-date/index.js
+++ src/date-time-picker/_example/year-month-date/index.js
@@ -8,6 +8,12 @@ Page({
     // 指定选择区间起始值
     start: '2000-01-01 00:00:00',
     end: '2030-09-09 12:12:12',
+    filter(type, options) {
+      if (type === 'year') {
+        return options.sort((a, b) => b.value - a.value);
+      }
+      return options;
+    },
   },
   showPicker(e) {
     const { mode } = e.currentTarget.dataset;
