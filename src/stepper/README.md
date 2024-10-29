55a5e3200bc0f578e311d35862bbdc50d37e7049
Y. <anly_yaw@163.com>
2024-10-29 11:01:27 +0800

    feat(Stepper): support integer props (#3230)
    * feat(Stepper): support integer props
    
    * test: update snapshots

--- src/stepper/README.md
+++ src/stepper/README.md
@@ -52,17 +52,21 @@ isComponent: true
 
 
 ## API
+
 ### Stepper Props
 
-名称 | 类型 | 默认值 | 说明 | 必传
+名称 | 类型 | 默认值 | 描述 | 必传
 -- | -- | -- | -- | --
+style | Object | - | 样式 | N
+custom-style | Object | - | 样式，一般用于开启虚拟化组件节点场景 | N
 disable-input | Boolean | false | 禁用输入框 | N
-disabled | Boolean | false | 禁用全部操作 | N
+disabled | Boolean | undefined | 禁用全部操作 | N
 input-width | Number | - | 输入框宽度，默认单位 `px` | N
+integer | Boolean | true | 是否整型 | N
 max | Number | 100 | 最大值 | N
 min | Number | 0 | 最小值 | N
+size | String | medium | 组件尺寸。可选项：small/medium/large。TS 类型：`SizeEnum`。[通用类型定义](https://github.com/Tencent/tdesign-miniprogram/blob/develop/src/common/common.ts) | N
 step | Number | 1 | 步长 | N
-size | String | medium | 组件尺寸。可选项：small/medium/large。TS 类型：`SizeEnum` | N
 theme | String | normal | 组件风格。可选项：normal/filled/outline | N
 value | String / Number | 0 | 值 | N
 default-value | String / Number | undefined | 值。非受控属性 | N
@@ -75,19 +79,21 @@ blur | `({ type: string \| number })` | 输入框失去焦点时触发
 change | `({ value: string \| number })` | 数值发生变更时触发
 overlimit | `({type: 'minus' \| 'plus'})` | 数值超出限制时触发
 
-### Stepper 外部样式类
-类名 | 说明
--- | -- 
+### Stepper External Classes
+
+类名 | 描述
+-- | --
 t-class | 根节点样式类
 t-class-input | 输入框样式类
 t-class-minus | 左侧递减号样式类
 t-class-plus | 右侧递增号样式类
 
-### CSS 变量
+### CSS Variables
+
 组件提供了下列 CSS 变量，可用于自定义样式。
 名称 | 默认值 | 描述 
 -- | -- | --
 --td-stepper-border-color | @component-border | - 
 --td-stepper-border-radius | @radius-small | - 
 --td-stepper-input-color | @font-gray-1 | - 
---td-stepper-input-disabled-color | @font-gray-4 | - 
+--td-stepper-input-disabled-color | @font-gray-4 | -
--- src/stepper/stepper.ts
+++ src/stepper/stepper.ts
@@ -42,65 +42,79 @@ export default class Stepper extends SuperComponent {
     },
   };
 
-  isDisabled(type) {
-    const { min, max, disabled } = this.properties;
-    const { currentValue } = this.data as any;
-    if (disabled) {
-      return true;
-    }
-    if (type === 'minus' && currentValue <= min) {
-      return true;
-    }
-    if (type === 'plus' && currentValue >= max) {
-      return true;
-    }
-    return false;
-  }
-
-  getLen(num: number) {
-    const numStr = num.toString();
-    return numStr.indexOf('.') === -1 ? 0 : numStr.split('.')[1].length;
-  }
-
-  add(a: number, b: number) {
-    const maxLen = Math.max(this.getLen(a), this.getLen(b));
-    const base = 10 ** maxLen;
-    return Math.round(a * base + b * base) / base;
-  }
-
-  format(value) {
-    const { min, max, step } = this.properties as any;
-    const len = Math.max(this.getLen(step), this.getLen(value));
-    // 超过边界取边界值
-    return Math.max(Math.min(max, value, Number.MAX_SAFE_INTEGER), min, Number.MIN_SAFE_INTEGER).toFixed(len);
-  }
-
-  setValue(value) {
-    value = this.format(value);
-    if (this.preValue === value) return;
-    this.preValue = value;
-    this._trigger('change', { value: Number(value) });
-  }
-
-  minusValue() {
-    if (this.isDisabled('minus')) {
-      this.triggerEvent('overlimit', { type: 'minus' });
-      return false;
-    }
-    const { currentValue, step } = this.data as any;
-    this.setValue(this.add(currentValue, -step));
-  }
-
-  plusValue() {
-    if (this.isDisabled('plus')) {
-      this.triggerEvent('overlimit', { type: 'plus' });
-      return false;
-    }
-    const { currentValue, step } = this.data as any;
-    this.setValue(this.add(currentValue, step));
-  }
-
   methods = {
+    isDisabled(type) {
+      const { min, max, disabled } = this.properties;
+      const { currentValue } = this.data as any;
+      if (disabled) {
+        return true;
+      }
+      if (type === 'minus' && currentValue <= min) {
+        return true;
+      }
+      if (type === 'plus' && currentValue >= max) {
+        return true;
+      }
+      return false;
+    },
+
+    getLen(num: number) {
+      const numStr = num.toString();
+      return numStr.indexOf('.') === -1 ? 0 : numStr.split('.')[1].length;
+    },
+
+    add(a: number, b: number) {
+      const maxLen = Math.max(this.getLen(a), this.getLen(b));
+      const base = 10 ** maxLen;
+      return Math.round(a * base + b * base) / base;
+    },
+
+    format(value) {
+      const { min, max, step } = this.properties as any;
+      const len = Math.max(this.getLen(step), this.getLen(value));
+      // 超过边界取边界值
+      return Math.max(Math.min(max, value, Number.MAX_SAFE_INTEGER), min, Number.MIN_SAFE_INTEGER).toFixed(len);
+    },
+
+    setValue(value) {
+      value = this.format(value);
+      if (this.preValue === value) return;
+      this.preValue = value;
+      this._trigger('change', { value: Number(value) });
+    },
+
+    minusValue() {
+      if (this.isDisabled('minus')) {
+        this.triggerEvent('overlimit', { type: 'minus' });
+        return false;
+      }
+      const { currentValue, step } = this.data as any;
+      this.setValue(this.add(currentValue, -step));
+    },
+
+    plusValue() {
+      if (this.isDisabled('plus')) {
+        this.triggerEvent('overlimit', { type: 'plus' });
+        return false;
+      }
+      const { currentValue, step } = this.data as any;
+      this.setValue(this.add(currentValue, step));
+    },
+
+    filterIllegalChar(value: string | number) {
+      const v = String(value).replace(/[^0-9.]/g, '');
+      const indexOfDot = v.indexOf('.');
+      if (this.properties.integer && indexOfDot !== -1) {
+        return v.split('.')[0];
+      }
+
+      if (!this.properties.integer && indexOfDot !== -1 && indexOfDot !== v.lastIndexOf('.')) {
+        return v.split('.', 2).join('.');
+      }
+
+      return v;
+    },
+
     handleFocus(e) {
       const { value } = e.detail;
 
@@ -114,7 +128,11 @@ export default class Stepper extends SuperComponent {
         return;
       }
 
-      this.triggerEvent('input', { value });
+      const formatted = this.filterIllegalChar(value);
+      this.setData({
+        currentValue: formatted,
+      });
+      this.triggerEvent('input', { value: formatted });
     },
 
     handleBlur(e) {
