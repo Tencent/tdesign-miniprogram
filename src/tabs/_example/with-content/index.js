e4238cfc4cd80add71c1b1ae2b0fa661c5eeec08
Y. <anly_yaw@163.com>
2024-11-13 20:24:40 +0800

    feat(TabPanel): panel height is determined by its content (#3280)
    * feat(TabPanel): panel height is determined by its content
    
    * feat(TabPanel): update demo
    
    * test: update snapshots

--- src/tabs/_example/with-content/index.js
+++ src/tabs/_example/with-content/index.js
@@ -1,7 +1,4 @@
 Component({
-  data: {
-    tabPanelstyle: 'display:flex;justify-content:center;align-items:center;',
-  },
   methods: {
     onTabsChange(event) {
       console.log(`Change tab, tab-panel value is ${event.detail.value}.`);
