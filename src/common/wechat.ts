e7c85e202c31b10429f6dd4444e3e3854b6299ae
Y. <anly_yaw@163.com>
2024-11-13 19:58:21 +0800

    chore: compatible with single page mode (#3281)
    * chore: compatible with single page mode
    
    * fix: fix type error
    
    * fix: fix cr

--- src/common/wechat.ts
+++ src/common/wechat.ts
@@ -9,14 +9,20 @@ export const getObserver = (context, selector: string) => {
   });
 };
 
+/**
+ * 背景：单页模式下, getWindowInfo、getAppBaseInfo、getDeviceInfo 等接口均返回 undefined。
+ * 复现路径：分享到朋友圈，再打开单页模式的该页面，wx.getWindowInfo() 等接口返回 undefined
+ * 代码片段：https://developers.weixin.qq.com/s/mzvZ8FmH7vVW
+ */
+
 export const getWindowInfo = () => {
-  return wx.getWindowInfo ? wx.getWindowInfo() : wx.getSystemInfoSync();
+  return wx.getWindowInfo ? wx.getWindowInfo() || wx.getSystemInfoSync() : wx.getSystemInfoSync();
 };
 
 export const getAppBaseInfo = () => {
-  return wx.getAppBaseInfo ? wx.getAppBaseInfo() : wx.getSystemInfoSync();
+  return wx.getAppBaseInfo ? wx.getAppBaseInfo() || wx.getSystemInfoSync() : wx.getSystemInfoSync();
 };
 
 export const getDeviceInfo = () => {
-  return wx.getDeviceInfo ? wx.getDeviceInfo() : wx.getSystemInfoSync();
+  return wx.getDeviceInfo ? wx.getDeviceInfo() || wx.getSystemInfoSync() : wx.getSystemInfoSync();
 };
