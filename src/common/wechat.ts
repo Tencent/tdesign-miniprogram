6031f1a98c9cfef01131b2442b17e087d807c4e2
anlyyao <anly_yaw@163.com>
2024-11-13 17:37:20 +0800

    fix: fix cr

--- src/common/wechat.ts
+++ src/common/wechat.ts
@@ -16,13 +16,13 @@ export const getObserver = (context, selector: string) => {
  */
 
 export const getWindowInfo = () => {
-  return wx.getWindowInfo && wx.getWindowInfo() ? wx.getWindowInfo() : wx.getSystemInfoSync();
+  return wx.getWindowInfo ? wx.getWindowInfo() || wx.getSystemInfoSync() : wx.getSystemInfoSync();
 };
 
 export const getAppBaseInfo = () => {
-  return wx.getAppBaseInfo && wx.getAppBaseInfo() ? wx.getAppBaseInfo() : wx.getSystemInfoSync();
+  return wx.getAppBaseInfo ? wx.getAppBaseInfo() || wx.getSystemInfoSync() : wx.getSystemInfoSync();
 };
 
 export const getDeviceInfo = () => {
-  return wx.getDeviceInfo && wx.getDeviceInfo() ? wx.getDeviceInfo() : wx.getSystemInfoSync();
+  return wx.getDeviceInfo ? wx.getDeviceInfo() || wx.getSystemInfoSync() : wx.getSystemInfoSync();
 };
