898b817b11c6fe2c9d10839e91af4a6ae2030de3
SinzoL <3013749951@qq.com>
2025-07-17 14:52:30 +0800

    feat(qrcode): 清除部分无用代码、修复iconSize属性问题

--- src/qrcode/qrcode.ts
+++ src/qrcode/qrcode.ts
@@ -1,7 +1,6 @@
-import { SuperComponent, wxComponent } from '../common/src/index';
-import config from '../common/config';
 import props from './props';
-import { TdQRCodeProps } from './type';
+import config from '../common/config';
+import { SuperComponent, wxComponent } from '../common/src/index';
 
 const { prefix } = config;
 const name = `${prefix}-qrcode`;
@@ -14,41 +13,48 @@ export default class QRCode extends SuperComponent {
     multipleSlots: true,
   };
 
-  properties = props as TdQRCodeProps;
+  properties = {
+    ...props,
+    statusRender :{ 
+      type: Boolean,
+      value: false
+    }
+  };
 
   data = {
     prefix,
+    showMask: false,
     classPrefix: name,
     canvasReady: false,
-    showMask: false,
   };
 
   lifetimes = {
     attached() {
-      this.setData({ showMask: this.properties.status !== 'active' });
+      this.setData({ 
+        showMask: this.properties.status !== 'active' 
+      });
     }
   };
 
   observers = {
     'status': function(newVal: string) {
-      this.setData({ showMask: newVal !== 'active' });
+      this.setData({ 
+        showMask: newVal !== 'active' 
+      });
     }
   };
 
   methods = {
-    qrcodeTouch() {
-      console.log("点击二维码");
-    },
     handleDrawCompleted() { 
-      console.log("二维码绘制成功");
-      this.setData({ canvasReady: true });
+      this.setData({ 
+        canvasReady: true 
+      });
     },
     handleDrawError(e: any) {
       const { error, detail } = e.detail;
       console.log("二维码绘制失败", error, detail);
     },
     handleRefresh() {
-      console.log("点击refresh");
       this.triggerEvent("onRefresh");
     }
   };
