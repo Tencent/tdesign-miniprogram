58184e4e992df8d34d2dfbab0c14d48bae0edef8
yichenhuan <heng_huan@126.com>
2025-09-08 15:58:25 +0800

    feat(test): 添加表单组件的单元测试，覆盖 props 和方法的各个场景

---
+++ packages/components/form/__test__/index.test.js
@@ -0,0 +1,408 @@
+import path from 'path';
+import simulate from 'miniprogram-simulate';
+
+describe('form', () => {
+  const form = load(path.resolve(__dirname, `../form`), 't-form');
+
+  describe('props', () => {
+    it(`: style && customStyle`, async () => {
+      const id = simulate.load({
+        template: `<t-form class="form" style="{{style}}" customStyle="{{customStyle}}"></t-form>`,
+        data: {
+          style: 'color: red',
+          customStyle: 'font-size: 9px',
+        },
+        usingComponents: {
+          't-form': form,
+        },
+      });
+      const comp = simulate.render(id);
+      comp.attach(document.createElement('parent-wrapper'));
+      const $form = comp.querySelector('.form >>> .t-form');
+      // expect(comp.toJSON()).toMatchSnapshot();
+      if (VIRTUAL_HOST) {
+        expect($form.dom.getAttribute('style').includes(`${comp.data.style}; ${comp.data.customStyle}`)).toBeTruthy();
+      } else {
+        expect($form.dom.getAttribute('style').includes(`${comp.data.customStyle}`)).toBeTruthy();
+      }
+    });
+
+    it(': labelAlign', async () => {
+      const id = simulate.load({
+        template: `<t-form class="form" labelAlign="{{labelAlign}}"></t-form>`,
+        data: {
+          labelAlign: 'left',
+        },
+        usingComponents: {
+          't-form': form,
+        },
+      });
+      const comp = simulate.render(id);
+      comp.attach(document.createElement('parent-wrapper'));
+
+      const $form = comp.querySelector('.form >>> .t-form');
+      expect($form.dom.getAttribute('class').includes('t-form--left')).toBeTruthy();
+
+      comp.setData({ labelAlign: 'top' });
+      await simulate.sleep(10);
+      expect($form.dom.getAttribute('class').includes('t-form--top')).toBeTruthy();
+    });
+
+    it(': colon && labelWidth', async () => {
+      const id = simulate.load({
+        template: `<t-form class="form" colon="{{colon}}" labelWidth="{{labelWidth}}"></t-form>`,
+        data: {
+          colon: true,
+          labelWidth: '120px',
+        },
+        usingComponents: {
+          't-form': form,
+        },
+      });
+      const comp = simulate.render(id);
+      comp.attach(document.createElement('parent-wrapper'));
+
+      const component = comp.querySelector('.form');
+      expect(component.instance.data.colon).toBe(true);
+      expect(component.instance.data.labelWidth).toBe('120px');
+    });
+
+    it(': data && rules', async () => {
+      const id = simulate.load({
+        template: `<t-form class="form" data="{{data}}" rules="{{rules}}"></t-form>`,
+        data: {
+          data: {
+            name: 'tdesign',
+            age: 18,
+          },
+          rules: {
+            name: [{ required: true, message: '请输入姓名' }],
+            age: [{ required: true, message: '请输入年龄' }],
+          },
+        },
+        usingComponents: {
+          't-form': form,
+        },
+      });
+      const comp = simulate.render(id);
+      comp.attach(document.createElement('parent-wrapper'));
+
+      const component = comp.querySelector('.form');
+      expect(component.instance.data.formData).toEqual({
+        name: 'tdesign',
+        age: 18,
+      });
+      expect(component.instance.data.rules).toEqual({
+        name: [{ required: true, message: '请输入姓名' }],
+        age: [{ required: true, message: '请输入年龄' }],
+      });
+    });
+
+    it(': showErrorMessage && requiredMark', async () => {
+      const id = simulate.load({
+        template: `<t-form class="form" showErrorMessage="{{showErrorMessage}}" requiredMark="{{requiredMark}}"></t-form>`,
+        data: {
+          showErrorMessage: false,
+          requiredMark: false,
+        },
+        usingComponents: {
+          't-form': form,
+        },
+      });
+      const comp = simulate.render(id);
+      comp.attach(document.createElement('parent-wrapper'));
+
+      const component = comp.querySelector('.form');
+      expect(component.instance.data.showErrorMessage).toBe(false);
+      expect(component.instance.data.requiredMark).toBe(false);
+    });
+
+    it(': resetType', async () => {
+      const id = simulate.load({
+        template: `<t-form class="form" resetType="{{resetType}}"></t-form>`,
+        data: {
+          resetType: 'initial',
+        },
+        usingComponents: {
+          't-form': form,
+        },
+      });
+      const comp = simulate.render(id);
+      comp.attach(document.createElement('parent-wrapper'));
+
+      const component = comp.querySelector('.form');
+      expect(component.instance.data.resetType).toBe('initial');
+    });
+  });
+
+  describe('methods', () => {
+    it(': validate', async () => {
+      const id = simulate.load({
+        template: `<t-form class="form" data="{{data}}" rules="{{rules}}"></t-form>`,
+        data: {
+          data: {
+            name: '',
+            age: 18,
+          },
+          rules: {
+            name: [{ required: true, message: '请输入姓名' }],
+            age: [{ required: true, message: '请输入年龄' }],
+          },
+        },
+        usingComponents: {
+          't-form': form,
+        },
+      });
+      const comp = simulate.render(id);
+      comp.attach(document.createElement('parent-wrapper'));
+
+      const component = comp.querySelector('.form');
+      const result = await component.instance.validate();
+      expect(result).toBeDefined();
+    });
+
+    it(': validateOnly', async () => {
+      const id = simulate.load({
+        template: `<t-form class="form" data="{{data}}" rules="{{rules}}"></t-form>`,
+        data: {
+          data: {
+            name: 'tdesign',
+            age: 18,
+          },
+          rules: {
+            name: [{ required: true, message: '请输入姓名' }],
+            age: [{ required: true, message: '请输入年龄' }],
+          },
+        },
+        usingComponents: {
+          't-form': form,
+        },
+      });
+      const comp = simulate.render(id);
+      comp.attach(document.createElement('parent-wrapper'));
+
+      const component = comp.querySelector('.form');
+      const result = await component.instance.validateOnly({ fields: ['name'] });
+      expect(result).toBeDefined();
+    });
+
+    it(': submit', async () => {
+      const handleSubmit = jest.fn();
+      const id = simulate.load({
+        template: `<t-form class="form" data="{{data}}" bind:submit="handleSubmit"></t-form>`,
+        data: {
+          data: {
+            name: 'tdesign',
+            age: 18,
+          },
+        },
+        methods: {
+          handleSubmit,
+        },
+        usingComponents: {
+          't-form': form,
+        },
+      });
+      const comp = simulate.render(id);
+      comp.attach(document.createElement('parent-wrapper'));
+
+      const component = comp.querySelector('.form');
+      const result = await component.instance.submit();
+      expect(result).toBeDefined();
+    });
+
+    it(': reset', async () => {
+      const handleReset = jest.fn();
+      const id = simulate.load({
+        template: `<t-form class="form" data="{{data}}" bind:reset="handleReset"></t-form>`,
+        data: {
+          data: {
+            name: 'tdesign',
+            age: 18,
+          },
+        },
+        methods: {
+          handleReset,
+        },
+        usingComponents: {
+          't-form': form,
+        },
+      });
+      const comp = simulate.render(id);
+      comp.attach(document.createElement('parent-wrapper'));
+
+      const component = comp.querySelector('.form');
+      component.instance.reset();
+      expect(handleReset).toHaveBeenCalled();
+    });
+
+    it(': clearValidate', async () => {
+      const id = simulate.load({
+        template: `<t-form class="form" data="{{data}}" rules="{{rules}}"></t-form>`,
+        data: {
+          data: {
+            name: 'tdesign',
+            age: 18,
+          },
+          rules: {
+            name: [{ required: true, message: '请输入姓名' }],
+            age: [{ required: true, message: '请输入年龄' }],
+          },
+        },
+        usingComponents: {
+          't-form': form,
+        },
+      });
+      const comp = simulate.render(id);
+      comp.attach(document.createElement('parent-wrapper'));
+
+      const component = comp.querySelector('.form');
+      component.instance.clearValidate(['name']);
+      // 验证清空验证结果的方法被调用
+      expect(component.instance.clearValidate).toBeDefined();
+    });
+
+    it(': setValidateMessage', async () => {
+      const id = simulate.load({
+        template: `<t-form class="form" data="{{data}}"></t-form>`,
+        data: {
+          data: {
+            name: 'tdesign',
+            age: 18,
+          },
+        },
+        usingComponents: {
+          't-form': form,
+        },
+      });
+      const comp = simulate.render(id);
+      comp.attach(document.createElement('parent-wrapper'));
+
+      const component = comp.querySelector('.form');
+      component.instance.setValidateMessage({
+        name: '自定义错误信息',
+      });
+      // 验证设置验证信息的方法被调用
+      expect(component.instance.setValidateMessage).toBeDefined();
+    });
+
+    it(': updateFormData', async () => {
+      const id = simulate.load({
+        template: `<t-form class="form" data="{{data}}"></t-form>`,
+        data: {
+          data: {
+            name: 'tdesign',
+            age: 18,
+          },
+        },
+        usingComponents: {
+          't-form': form,
+        },
+      });
+      const comp = simulate.render(id);
+      comp.attach(document.createElement('parent-wrapper'));
+
+      const component = comp.querySelector('.form');
+      component.instance.updateFormData('name', 'new name');
+      expect(component.instance.data.formData.name).toBe('new name');
+    });
+
+    it(': getEmptyValue', async () => {
+      const id = simulate.load({
+        template: `<t-form class="form" data="{{data}}"></t-form>`,
+        data: {
+          data: {
+            name: 'tdesign',
+            age: 18,
+            tags: ['tag1', 'tag2'],
+            info: { key: 'value' },
+          },
+        },
+        usingComponents: {
+          't-form': form,
+        },
+      });
+      const comp = simulate.render(id);
+      comp.attach(document.createElement('parent-wrapper'));
+
+      const component = comp.querySelector('.form');
+
+      // 测试字符串类型
+      const stringEmpty = component.instance.getEmptyValue('name');
+      expect(stringEmpty).toBe('');
+
+      // 测试数字类型
+      const numberEmpty = component.instance.getEmptyValue('age');
+      expect(numberEmpty).toBe(0);
+
+      // 测试数组类型
+      const arrayEmpty = component.instance.getEmptyValue('tags');
+      expect(arrayEmpty).toEqual([]);
+
+      // 测试对象类型
+      const objectEmpty = component.instance.getEmptyValue('info');
+      expect(objectEmpty).toEqual({});
+    });
+  });
+
+  describe('event', () => {
+    it(': submit', async () => {
+      const handleSubmit = jest.fn();
+      const id = simulate.load({
+        template: `<t-form class="form" bind:submit="handleSubmit"></t-form>`,
+        methods: {
+          handleSubmit,
+        },
+        usingComponents: {
+          't-form': form,
+        },
+      });
+      const comp = simulate.render(id);
+      comp.attach(document.createElement('parent-wrapper'));
+
+      const $form = comp.querySelector('.form >>> form');
+      $form.dispatchEvent('submit');
+      await simulate.sleep(10);
+      expect(handleSubmit).toHaveBeenCalled();
+    });
+
+    it(': reset', async () => {
+      const handleReset = jest.fn();
+      const id = simulate.load({
+        template: `<t-form class="form" bind:reset="handleReset"></t-form>`,
+        methods: {
+          handleReset,
+        },
+        usingComponents: {
+          't-form': form,
+        },
+      });
+      const comp = simulate.render(id);
+      comp.attach(document.createElement('parent-wrapper'));
+
+      const $form = comp.querySelector('.form >>> form');
+      $form.dispatchEvent('reset');
+      await simulate.sleep(10);
+      expect(handleReset).toHaveBeenCalled();
+    });
+
+    it(': validate', async () => {
+      const handleValidate = jest.fn();
+      const id = simulate.load({
+        template: `<t-form class="form" bind:validate="handleValidate"></t-form>`,
+        methods: {
+          handleValidate,
+        },
+        usingComponents: {
+          't-form': form,
+        },
+      });
+      const comp = simulate.render(id);
+      comp.attach(document.createElement('parent-wrapper'));
+
+      const component = comp.querySelector('.form');
+      await component.instance.validate();
+      expect(handleValidate).toHaveBeenCalled();
+    });
+  });
+});
