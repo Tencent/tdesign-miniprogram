56dedc7977da5eb155c7ac6f6afc17139e7a9ac8
anlyyao <anly_yaw@163.com>
2025-09-11 05:06:09 +0800

    feat(site): support chat site

---
+++ packages/tdesign-miniprogram-chat/CHANGELOG.md
@@ -0,0 +1,9 @@
+---
+title: 更新日志
+spline: explain
+toc: false
+docClass: timeline
+---
+
+
+## 🌈 首发
---
+++ packages/tdesign-miniprogram-chat/site/vite.config.ts
@@ -0,0 +1,67 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { defineConfig } from 'vite';
+
+import rollupResolve from '@rollup/plugin-node-resolve';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+
+import changelog2Json from './plugins/changelog-to-json';
+import createTDesignPlugin from './plugins/plugin-tdoc';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const publicPathMap: Record<string, string> = {
+  preview: '/',
+  intranet: '/miniprogram/',
+  production: 'https://static.tdesign.tencent.com/miniprogram/',
+};
+
+// https://vitejs.dev/config/
+export default ({ mode }: any) => {
+  return defineConfig({
+    base: publicPathMap[mode],
+    root: path.resolve(__dirname),
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname, './'),
+        '@': path.resolve(__dirname, '../../components'),
+        '@common': path.resolve(__dirname, '../../common'),
+        '@components': path.resolve(__dirname, '../../components'),
+        '@docs': path.resolve(__dirname, './docs'),
+        '@pages': path.resolve(__dirname, './pages'),
+        '@tdesign-miniprogram': path.resolve(__dirname, '../../tdesign-miniprogram'),
+      },
+    },
+    server: {
+      host: '127.0.0.1',
+      port: 19000,
+      open: '/',
+      https: false,
+    },
+    build: {
+      rollupOptions: {
+        input: {
+          site: path.resolve(__dirname, 'index.html'),
+        },
+        plugins: [
+          rollupResolve({
+            moduleDirectories: [path.resolve(__dirname, 'node_modules')],
+          }),
+        ],
+      },
+    },
+    plugins: [
+      vue({
+        template: {
+          compilerOptions: {
+            isCustomElement: (tag) => tag.startsWith('td-'),
+          },
+        },
+      }),
+      vueJsx(),
+      createTDesignPlugin(),
+      changelog2Json(),
+    ],
+  });
+};
