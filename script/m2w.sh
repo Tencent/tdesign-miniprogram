bfc0e8147755ec53ddb92ba309f97b8f85947018
anlyyao <anly_yaw@163.com>
2025-09-16 19:26:32 +0800

    chore: update scripts

--- script/m2w.sh
+++ script/m2w.sh
@@ -6,7 +6,12 @@ npm run build
 # 切换到 _example 目录
 cd _example
 
-pnpm add @tencent/m2w-tdesign -w --registry http://mirrors.tencent.com/npm/
+# 切换腾讯源
+npm config set registry http://mirrors.tencent.com/npm/
+
+pnpm install
+
+pnpm add @tencent/m2w-tdesign@latest -w
 
 # 执行 m2w-tdesign 命令
 npx m2w-tdesign build --navigationStyle custom --output ../packages/tdesign-miniprogram/site/dist/live --baseUrl /miniprogram/live
