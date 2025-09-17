1b062197bf0ee21320ab0bde8809031a9bb43831
anlyyao <anly_yaw@163.com>
2025-09-17 17:14:39 +0800

    feat(site): add AI Chat routing

--- packages/tdesign-miniprogram/example/pages/home/data/chat.ts
+++ packages/tdesign-miniprogram/example/pages/home/data/chat.ts
@@ -2,38 +2,38 @@ const chat = {
   name: 'AI Chat 对话',
   icon: 'chat',
   childArr: [
-    // {
-    //   name: 'Chat',
-    //   label: '对话',
-    // },
-    // {
-    //   name: 'ChatMessage',
-    //   label: '对话单元',
-    // },
-    // {
-    //   name: 'ChatSender',
-    //   label: '对话输入框',
-    // },
-    // {
-    //   name: 'ChatMarkdown',
-    //   label: '对话 Markdown',
-    // },
-    // {
-    //   name: 'ChatThinking',
-    //   label: '思考过程',
-    // },
-    // {
-    //   name: 'ChatAttachments',
-    //   label: '附件',
-    // },
-    // {
-    //   name: 'ChatActionbar',
-    //   label: '对话操作栏',
-    // },
-    // {
-    //   name: 'ChatLoading',
-    //   label: '加载中',
-    // },
+    {
+      name: 'Chat',
+      label: '对话',
+    },
+    {
+      name: 'ChatMessage',
+      label: '对话单元',
+    },
+    {
+      name: 'ChatSender',
+      label: '对话输入框',
+    },
+    {
+      name: 'ChatMarkdown',
+      label: '对话 Markdown',
+    },
+    {
+      name: 'ChatThinking',
+      label: '思考过程',
+    },
+    {
+      name: 'ChatAttachments',
+      label: '附件',
+    },
+    {
+      name: 'ChatActionbar',
+      label: '对话操作栏',
+    },
+    {
+      name: 'ChatLoading',
+      label: '加载中',
+    },
   ],
 };
 
