2e8c456caed448d7832c06ad5aa9a8f34e8afb28
novlan1 <1576271227@qq.com>
2025-11-19 23:06:13 +0800

    chore: remove useless script

--- packages/tdesign-uniapp/example/script/deploy/local.env.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-
-function readLocalEnv() {
-  const configPath =  path.resolve(process.cwd(), './.env.local');
-  require('dotenv').config({ path: configPath });
-}
-
-module.exports = readLocalEnv;
