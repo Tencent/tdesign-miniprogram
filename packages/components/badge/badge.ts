a383a2fdc3f66139b25db95c6b23a1a0dae660b5
Y. <anly_yaw@163.com>
2025-12-25 14:41:01 +0800

    feat(Badge): the shape attr add four optional values (#4137)
    * feat(Badge): the shape attr add four optional values
    
    * format: adjust font variable line breaks in _font.less
    
    * feat(Badge): update demo
    
    * fix: fix cr
    
    * docs: regenerate api docs
    
    * fix: fix demo
    
    * chore(Badge): optimze ribbon badge
    
    * fix: fix cr

--- packages/components/badge/badge.ts
+++ packages/components/badge/badge.ts
@@ -2,7 +2,7 @@ import { SuperComponent, wxComponent } from '../common/src/index';
 import config from '../common/config';
 import props from './props';
 import type { TdBadgeProps } from './type';
-import { uniqueFactory } from '../common/utils';
+import { uniqueFactory, getRect } from '../common/utils';
 
 const { prefix } = config;
 const name = `${prefix}-badge`;
@@ -26,14 +26,33 @@ export default class Badge extends SuperComponent {
     value: '',
     labelID: '',
     descriptionID: '',
+    useOuterClass: false,
   };
 
   lifetimes = {
     ready() {
       const uniqueID = getUniqueID();
+
       this.setData({
         labelID: `${uniqueID}_label`,
         descriptionID: `${uniqueID}_description`,
+      });
+
+      this.checkForActualContent();
+    },
+  };
+
+  methods = {
+    checkForActualContent() {
+      const target = ['ribbon', 'ribbon-right', 'ribbon-left', 'triangle-right', 'triangle-left'];
+      if (this.properties.content || !target.includes(this.properties.shape)) {
+        this.setData({ useOuterClass: false });
+        return;
+      }
+
+      return getRect(this, `.${name}__content`).then((rect) => {
+        const hasSlotContent = rect.width > 0 || rect.height > 0;
+        this.setData({ useOuterClass: !hasSlotContent });
       });
     },
   };
