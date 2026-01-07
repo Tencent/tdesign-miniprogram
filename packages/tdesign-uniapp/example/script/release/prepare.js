60d54f1b760936f4b74686d046b468f93e344ced
黎伟杰 <674416404@qq.com>
2026-01-07 15:40:35 +0800

    chore: fix cr

--- packages/tdesign-uniapp/example/script/release/prepare.js
+++ packages/tdesign-uniapp/example/script/release/prepare.js
@@ -30,13 +30,12 @@ async function main() {
 
 async function prepareOne({ targetDir, sourceGlob, sourceDir }) {
   deleteFolder(targetDir);
-  const list = glob
-    .sync(sourceGlob, {
-      ignore: '**/{node_modules,_example}/**/*',
-      nodir: true,
-      dot: true,
-    })
-    .filter((item) => !item.includes('_example') && !item.includes('node_modules') && !item.includes('package.json'));
+  const list = glob.sync(sourceGlob, {
+    cwd: sourceDir,
+    ignore: ['**/{node_modules,_example}/**/*', 'package.json'],
+    nodir: true,
+    dot: true,
+  });
 
   for (const item of list) {
     const relativePath = path.relative(sourceDir, item);
