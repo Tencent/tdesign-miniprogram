f1a006f65864e8e7c1f6ffa8208ea991986d0494
novlan1 <1576271227@qq.com>
2026-01-25 00:59:22 +0800

    feat(calendar): support allowSameDay

--- packages/uniapp-components/calendar/calendar.vue
+++ packages/uniapp-components/calendar/calendar.vue
@@ -8,7 +8,6 @@
       placement="bottom"
       @visible-change="onVisibleChange"
     >
-      <!-- parse <include src="./template.wxml"/> -->
       <CalendarTemplate
         :class-prefix="classPrefix"
         :use-popup="usePopup"
@@ -39,7 +38,6 @@
       </CalendarTemplate>
     </t-popup>
     <block v-else>
-      <!-- parse <include src="./template.wxml"/> -->
       <CalendarTemplate
         :class-prefix="classPrefix"
         :use-popup="usePopup"
@@ -164,6 +162,10 @@ export default uniComponent({
       },
     },
 
+    allowSameDay(v) {
+      this.base.allowSameDay = v;
+    },
+
     confirmBtn: {
       handler(v) {
         if (typeof v === 'string') {
@@ -198,9 +200,8 @@ export default uniComponent({
       handler(v) {
         this.base.value = v;
         this.calcMonths();
-        this.updateCurrentMonth(v);
+        this.updateCurrentMonth(Array.isArray(v) ? v[0] : v);
       },
-      // immediate: true,
       deep: true,
     },
 
@@ -292,14 +293,16 @@ export default uniComponent({
     updateActionButton(value) {
       const _min = this.getCurrentYearAndMonth(this.base.minDate);
       const _max = this.getCurrentYearAndMonth(this.base.maxDate);
+      const _value = this.getCurrentYearAndMonth(value);
 
       const _minTimestamp = new Date(_min.year, _min.month, 1).getTime();
       const _maxTimestamp = new Date(_max.year, _max.month, 1).getTime();
+      const _dateValue = new Date(_value.year, _value.month, 1);
 
-      const _prevYearTimestamp = getPrevYear(value).getTime();
-      const _prevMonthTimestamp = getPrevMonth(value).getTime();
-      const _nextMonthTimestamp = getNextMonth(value).getTime();
-      const _nextYearTimestamp = getNextYear(value).getTime();
+      const _prevYearTimestamp = getPrevYear(_dateValue).getTime();
+      const _prevMonthTimestamp = getPrevMonth(_dateValue).getTime();
+      const _nextMonthTimestamp = getNextMonth(_dateValue).getTime();
+      const _nextYearTimestamp = getNextYear(_dateValue).getTime();
 
       const preYearBtnDisable = _prevYearTimestamp < _minTimestamp || _prevMonthTimestamp < _minTimestamp;
       const prevMonthBtnDisable = _prevMonthTimestamp < _minTimestamp;
--- packages/uniapp-components/common/shared/calendar/index.js
+++ packages/uniapp-components/common/shared/calendar/index.js
@@ -45,11 +45,12 @@ export default class TCalendar {
     const { year: maxYear, month: maxMonth, time: maxTime } = getDateRect(maxDate);
     const calcType = (year, month, date) => {
       const curDate = new Date(year, month, date, 23, 59, 59);
+
       if (type === 'single' && selectedDate) {
         if (isSameDate({ year, month, date }, selectedDate)) return 'selected';
       }
       if (type === 'multiple' && selectedDate) {
-        const hit = selectedDate.some(item => isSameDate({ year, month, date }, item));
+        const hit = selectedDate.some((item) => isSameDate({ year, month, date }, item));
         if (hit) {
           return 'selected';
         }
@@ -59,18 +60,22 @@ export default class TCalendar {
           const [startDate, endDate] = selectedDate;
           const compareWithStart = startDate && isSameDate({ year, month, date }, startDate);
           const compareWithEnd = endDate && isSameDate({ year, month, date }, endDate);
+
           if (compareWithStart && compareWithEnd && allowSameDay) return 'start-end';
           if (compareWithStart) return 'start';
           if (compareWithEnd) return 'end';
-          if (startDate && endDate && curDate.getTime() > startDate.getTime() && curDate.getTime() < endDate.getTime()) return 'centre';
+          if (startDate && endDate && curDate.getTime() > startDate.getTime() && curDate.getTime() < endDate.getTime())
+            return 'centre';
         }
       }
+
       const minCurDate = new Date(year, month, date, 0, 0, 0);
       if (curDate.getTime() < minTime || minCurDate.getTime() > maxTime) {
         return 'disabled';
       }
       return '';
     };
+
     while (minYear < maxYear || (minYear === maxYear && minMonth <= maxMonth)) {
       const target = getMonthDateRect(new Date(minYear, minMonth, 1));
       const months = [];
@@ -93,6 +98,7 @@ export default class TCalendar {
       minYear = curDate.year;
       minMonth = curDate.month;
     }
+  
     return ans;
   }
 
