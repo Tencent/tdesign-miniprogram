efc7ea1c3d4f1a198127259c5af93497b0d94dd7
nekowu <1142809915@qq.com>
2026-01-26 17:12:13 +0800

    chore: 暂时关闭rpx转换

--- packages/tdesign-uniapp/example/script/release/less.js
+++ packages/tdesign-uniapp/example/script/release/less.js
@@ -18,21 +18,24 @@ const CONFIG = {
 // 配置参数（通常 1rpx=0.5px，设计稿 750px 宽时）
 const options = {
   transformType: 'rpx',
-  rpxUnit: 0.5,      // 转换比例 1rpx = 0.5px
-  rpxPrecision: 6,    // 输出精度
+  rpxUnit: 0.5, // 转换比例 1rpx = 0.5px
+  rpxPrecision: 6, // 输出精度
 };
 
 // 处理流程
 async function processLess(inputFile, rawOutputFile, rawOutputFileInApp) {
   if (!inputFile.endsWith('.less')) return;
-  if (CONFIG.whiteList.find(item => inputFile.startsWith(item))) {
+  if (CONFIG.whiteList.find((item) => inputFile.startsWith(item))) {
     return;
   }
 
   try {
     let lessCode = fs.readFileSync(inputFile, 'utf8');
 
-    lessCode = lessCode.replace('@import \'tdesign-uniapp/common/style/base.less\'', '@import \'../common/style/base.less\'');
+    lessCode = lessCode.replace(
+      "@import 'tdesign-uniapp/common/style/base.less'",
+      "@import '../common/style/base.less'",
+    );
 
     const cssResult = await less.render(lessCode, {
       // 设置导入路径
@@ -45,21 +48,17 @@ async function processLess(inputFile, rawOutputFile, rawOutputFileInApp) {
     });
 
     const postcssResult = await postcss([
-      rpxTransform(options),
+      // rpxTransform(options),
     ]).process(cssResult.css, { from: undefined });
 
-
     const getOutputFile = (rawOutputFile) => {
-      const filename = `${path
-        .basename(rawOutputFile, path.extname(rawOutputFile))
-        .replace(/^_/, '')}.css`;
+      const filename = `${path.basename(rawOutputFile, path.extname(rawOutputFile)).replace(/^_/, '')}.css`;
 
       const outputFile = path.resolve(path.dirname(rawOutputFile), filename);
       console.log('filename', filename);
 
       return outputFile;
     };
-
 
     const outputFile = getOutputFile(rawOutputFile);
     fs.writeFileSync(outputFile, postcssResult.css);
