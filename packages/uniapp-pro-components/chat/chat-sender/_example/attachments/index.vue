3a5b3ca6e3b560c2dc7359a4934bc0a9dfff798e
novlan1 <1576271227@qq.com>
2026-02-04 11:27:20 +0800

    feat(icon): sync to uniapp

--- packages/uniapp-pro-components/chat/chat-sender/_example/attachments/index.vue
+++ packages/uniapp-pro-components/chat/chat-sender/_example/attachments/index.vue
@@ -6,8 +6,13 @@
           <view class="chat-sender-height-left-limit" />
           <view class="chat-sender-height-right-limit" />
         </view>
-        <view class="chat-sender-placeholder"> 高度限制：最大高度为132px </view>
-        <view class="chat-sender-wrapper" :style="showUploadMenu ? 'bottom:32rpx;' : ''">
+        <view class="chat-sender-placeholder">
+          高度限制：最大高度为132px
+        </view>
+        <view
+          class="chat-sender-wrapper"
+          :style="showUploadMenu ? 'bottom:32rpx;' : ''"
+        >
           <t-chat-sender
             :value="value"
             :loading="loading"
@@ -34,18 +39,37 @@
           >
             <template #footer-prefix>
               <view class="demo-footer-prefix">
-                <view :class="'deep-think-block ' + (deepThinkActive ? 'active' : '')" @click.stop="onDeepThinkTap">
-                  <t-icon name="system-sum" size="36rpx" />
-                  <text class="deep-think-text"> 深度思考 </text>
+                <view
+                  :class="'deep-think-block ' + (deepThinkActive ? 'active' : '')"
+                  @click.stop="onDeepThinkTap"
+                >
+                  <t-icon
+                    name="system-sum"
+                    size="36rpx"
+                  />
+                  <text class="deep-think-text">
+                    深度思考
+                  </text>
                 </view>
-                <view :class="'net-search-block ' + (netSearchActive ? 'active' : '')" @click.stop="onNetSearchTap">
-                  <t-icon name="internet" size="36rpx" />
+                <view
+                  :class="'net-search-block ' + (netSearchActive ? 'active' : '')"
+                  @click.stop="onNetSearchTap"
+                >
+                  <t-icon
+                    name="internet"
+                    size="36rpx"
+                  />
                 </view>
               </view>
             </template>
           </t-chat-sender>
         </view>
-        <view v-if="!visible" class="demo-attachments-footer"> 内容由AI生成，仅供参考 </view>
+        <view
+          v-if="!visible"
+          class="demo-attachments-footer"
+        >
+          内容由AI生成，仅供参考
+        </view>
       </view>
     </view>
     <t-toast ref="t-toast" />
