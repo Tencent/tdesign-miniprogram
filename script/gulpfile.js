e80fa435b9527746c510b39954388277bf772b21
anlyyao <anly_yaw@163.com>
2026-02-05 17:24:27 +0800

    chore: update gulp config

--- script/gulpfile.js
+++ script/gulpfile.js
@@ -2,9 +2,19 @@ const gulp = require('gulp');
 const dist = require('./gulpfile.dist');
 const example = require('./gulpfile.example');
 const wechatide = require('./gulpfile.wechatide');
+
+// TODO: 等 config-provider 正式上线，需要改用子仓库的语言包
+/** `gulp copyLocale`
+ * 复制 packages/common/js/global-config/mobile/locale 的 ts 文件到 packages/components/locale
+ * */
+// const copyLocale = () =>
+//   gulp.src('packages/common/js/global-config/mobile/locale/*.ts').pipe(gulp.dest('packages/components/locale'));
+
 /** `gulp build`
  * 构建
  * */
+// const build = gulp.series(copyLocale, dist.build, example.build('all'));
+
 const build = gulp.series(dist.build, example.build('all'));
 
 /** `gulp watch`
