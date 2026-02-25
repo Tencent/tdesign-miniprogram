af8c4c6a0bcb2c6dd63bbbe196cb662504a5b769
anlyyao <anly_yaw@163.com>
2026-02-25 17:27:45 +0800

    test(ConfigProvider): add unit test

---
+++ packages/components/config-provider/__test__/demo.test.js
@@ -0,0 +1,19 @@
+/**
+ * 该文件为由脚本 `npm run test:demo` 自动生成，如需修改，执行脚本命令即可。请勿手写直接修改，否则会被覆盖
+ */
+
+import path from 'path';
+import simulate from 'miniprogram-simulate';
+
+const mapper = ['base'];
+
+describe('ConfigProvider', () => {
+  mapper.forEach((demoName) => {
+    it(`ConfigProvider ${demoName} demo works fine`, () => {
+      const id = load(path.resolve(__dirname, `../../config-provider/_example/${demoName}/index`), demoName);
+      const container = simulate.render(id);
+      container.attach(document.createElement('parent-wrapper'));
+      expect(container.toJSON()).toMatchSnapshot();
+    });
+  });
+});
