b3b5dd289fe6e7adf3d6ad90f7b65940aa70efed
novlan1 <1576271227@qq.com>
2026-03-04 15:52:30 +0800

    docs: update docs (#4309)
    * docs: update docs
    
    * chore: support windows

--- packages/tdesign-uniapp/example/script/release/config.js
+++ packages/tdesign-uniapp/example/script/release/config.js
@@ -3,14 +3,19 @@ const path = require('path');
 const PACKAGES_ROOT = path.resolve(__dirname, '../../../../');
 const PROJECT_ROOT = path.resolve(PACKAGES_ROOT, '../');
 
+// 将 Windows 的 \ 路径分隔符转为 /，确保 glob pattern 跨平台兼容
+function toGlobPattern(p) {
+  return p.split(path.sep).join('/');
+}
+
 const config = {
   targetDir: path.resolve(PACKAGES_ROOT, 'tdesign-uniapp/dist'),
   sourceDir: path.resolve(PACKAGES_ROOT, 'uniapp-components'),
-  sourceGlob: path.resolve(PACKAGES_ROOT, 'uniapp-components/**/*'),
+  sourceGlob: toGlobPattern(path.resolve(PACKAGES_ROOT, 'uniapp-components/**/*')),
 
   chatTargetDir: path.resolve(PACKAGES_ROOT, 'tdesign-uniapp-chat/dist'),
   chatSourceDir: path.resolve(PACKAGES_ROOT, 'uniapp-pro-components/chat'),
-  chatSourceGlob: path.resolve(PACKAGES_ROOT, 'uniapp-pro-components/chat/**/*'),
+  chatSourceGlob: toGlobPattern(path.resolve(PACKAGES_ROOT, 'uniapp-pro-components/chat/**/*')),
 
   demoDir: path.resolve(__dirname, '../../src/pages-more'),
 };
@@ -25,4 +30,5 @@ module.exports = {
   PACKAGES_ROOT,
   PROJECT_ROOT,
   DIST_BLACK_LIST,
+  toGlobPattern,
 };
