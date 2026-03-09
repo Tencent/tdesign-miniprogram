fdc1813f777ce2f42558f6ff5097bd8b587f2ab7
zhangjiaoalice <2640847812@qq.com>
2026-03-09 15:03:59 +0800

    feat: chat-record 组件交互更改

--- packages/pro-components/chat/chat-record/props.ts
+++ packages/pro-components/chat/chat-record/props.ts
@@ -7,17 +7,22 @@
 import { TdChatRecordProps } from './type';
 
 const props: TdChatRecordProps = {
-  useSpeechInputSlot: {
+  /** 是否自动发送（预留扩展） */
+  autoSend: {
     type: Boolean,
     value: false,
   },
-  useSpeechNoAuthSlot: {
-    type: Boolean,
-    value: false,
+
+  /** 识别语言（WechatSI 插件参数） */
+  lang: {
+    type: String,
+    value: 'zh_CN',
   },
-  autoSendHeight: {
-    type: Boolean,
-    value: true,
+
+  /** 最大录音时长（ms） */
+  duration: {
+    type: Number,
+    value: 60000,
   },
 };
 
