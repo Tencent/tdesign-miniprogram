f7cb5f98a310a2ec2e1d48618547b0bf36c3f106
anlyyao <anly_yaw@163.com>
2026-03-18 19:04:05 +0800

    test: update snapshots

---
+++ packages/pro-components/chat/chat-thinking/__test__/demo.test.js
@@ -0,0 +1,19 @@
+/**
+ * 该文件为由脚本 `npm run test:demo` 自动生成，如需修改，执行脚本命令即可。请勿手写直接修改，否则会被覆盖
+ */
+
+import path from 'path';
+import simulate from 'miniprogram-simulate';
+
+const mapper = ['base', 'complete', 'layout', 'pending'];
+
+describe('ChatThinking', () => {
+  mapper.forEach((demoName) => {
+    it(`ChatThinking ${demoName} demo works fine`, () => {
+      const id = load(path.resolve(__dirname, `../../chat-thinking/_example/${demoName}/index`), demoName);
+      const container = simulate.render(id);
+      container.attach(document.createElement('parent-wrapper'));
+      expect(container.toJSON()).toMatchSnapshot();
+    });
+  });
+});
