a185a8c7e8631736346b55997ea92b4e65de5abb
mimaoxiao <34621576+mimaoxiao@users.noreply.github.com>
2026-03-24 15:33:44 +0800

    Feature/markdown click preview (#4374)
    * feat: node接受点击事件后返回自身数据
    
    * feat: 补充示例
    
    * chore: 更新子仓

--- packages/pro-components/chat/chat-markdown/_example/sheet/index.js
+++ packages/pro-components/chat/chat-markdown/_example/sheet/index.js
@@ -5,7 +5,15 @@ Page({
     markdownContent: markdownData,
   },
   handleNodeTap(e) {
+    const { node } = e.detail;
     // 打印节点信息
-    console.log('点击节点', e.detail.node);
+    console.log('点击节点', node);
+    // 图片节点预览
+    if (node && node.type === 'image') {
+      wx.previewImage({
+        urls: [node.href],
+        current: node.href,
+      });
+    }
   },
 });
